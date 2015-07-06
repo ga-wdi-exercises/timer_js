312fd2081b4b37d6d608bf13787424a28a39ce96
Erica Irving <contact.eirving@gmail.com>
2015-07-06 19:02:15 -0400

    eliminated problem where interval doubles if start clicked more than once

--- timers.js
+++ timers.js
@@ -1,10 +1,10 @@
-//Create Javascript selectors that target each of the timer buttons.
 var resetButton = document.getElementById("reset");
 var startButton = document.getElementById("start");
 var pauseButton = document.getElementById("pause");
 //Instantiate seconds and timerId variables for your timer. The latter will make more sense after reading up on setInterval().
 var seconds = 0;
-var timerId
+var timerId;
+var counter = 0;
 
 //Create an updateTime() function that increments the seconds counter and inserts that value into the <h1> element with id="timer".
 function updateTime() {
@@ -12,16 +12,19 @@ function updateTime() {
   document.getElementById("timer").innerHTML = "Time elapsed: " + seconds;
 };
 
-//Replace "Stop Watch" in the HTML with the content of the seconds variable.
-//Use setInterval() to increment the timer by 1 every second.
 startButton.addEventListener("click", function (){
-  timerId = setInterval(updateTime, 1000)
+  //Use setInterval() to increment the timer by 1 every second.
   //Make sure the timer starts back up when you hit the "Start" button after hitting "Pause".
+  counter++;
+  if (counter === 1){
+  timerId = setInterval(updateTime, 1000)
+  }
 });
 
 pauseButton.addEventListener("click", function(){
   //Stop -- but do not reset! -- the timer using clearInterval().
   clearInterval(timerId);
+  counter = 0;
   });
 
 resetButton.addEventListener("click", function(){
@@ -29,6 +32,7 @@ resetButton.addEventListener("click", function(){
   clearInterval(timerId);
   //Reset the timer.
   seconds = 0;
+  counter = 0;
   //Replace the time in your HTML with the original "Stop Watch" text.
   document.getElementById("timer").innerHTML = "Stop Watch";
 });
