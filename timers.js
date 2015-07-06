428ebfafb20d54f16c58fe91f4e42c6689703c67
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-06 18:38:51 -0400

    on start click timer displays increments in seconds. issue: multiple start clicks increase speed

--- timers.js
+++ timers.js
@@ -4,7 +4,12 @@ buttons.addEventListener("click", buttonClick);
 
 function buttonClick(){
   if (event.target.id == "start"){
-    document.getElementById("timer").innerHTML = "Time Elapsed: 0";
+    var stopwatchInterval = window.setInterval(increaseStopwatch, 1000);
   }
+}
 
+var stopwatchValue = 0;
+function increaseStopwatch(){
+  stopwatchValue ++;
+  return document.getElementById("timer").innerHTML = "Time elapsed: " + stopwatchValue;
 }
