8c5360d1cc22057434b17137408a44336076fa4b
Alex Bannon <alexbannon@gmail.com>
2015-07-06 18:28:29 -0400

    made working javascript timer with start at one object solution

--- timers.js
+++ timers.js
@@ -1,4 +1,49 @@
+var editTimer = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var resetButton = document.getElementById("reset");
+var pauseButton = document.getElementById("pause");
+var startSetInterval;
+var startingStopWatchNumber = 0;
+startingStopWatchNumber.toFixed(2);
+var pauseClicks = 1;
+startButton.addEventListener("click", startTimer);
+resetButton.addEventListener("click", stopTimer);
+pauseButton.addEventListener("click", pauseTimer);
+function startTimer() {
+  if(startingStopWatchNumber !== 0) {
+    clearInterval(timerId);
+    timerId = setInterval(timerIncreaseByOne, 100);
+    pauseClicks = 1;
+  }
+  else{
+    pauseClicks = 1;
+    timerId = setInterval(timerIncreaseByOne, 100);
+  }
+};
+function timerIncreaseByOne(){
+  var newNumber = startingStopWatchNumber = startingStopWatchNumber + 0.1;
+  var twoDigitNumber = newNumber.toFixed(1);
+  editTimer.innerHTML = twoDigitNumber + " Seconds";
 
-
-
-
+}
+function stopTimer() {
+  startButton.removeEventListener("click", startTimer);
+  clearInterval(timerId);
+  editTimer.innerHTML = "Stop Watch";
+  startButton.addEventListener("click", startTimer);
+  startingStopWatchNumber = 0;
+}
+function pauseTimer() {
+  pauseClicks++;
+  if(startingStopWatchNumber === 0) {
+    console.log("user tried to start watch with pause");
+  }
+  else if(pauseClicks % 2 === 0){
+    startButton.removeEventListener("click", startTimer);
+    clearInterval(timerId);
+    startButton.addEventListener("click", startTimer);
+  }
+  else {
+    timerId = setInterval(timerIncreaseByOne, 100);
+  }
+}
---
+++ timersOneObject.js
@@ -0,0 +1,15 @@
+/*var timer = {
+  editTimer: document.getElementById("timer"),
+  startButton: document.getElementById("start"),
+  resetButton: document.getElementById("reset"),
+  pauseButton: document.getElementById("pause")
+}
+
+var buttons = document.querySelector(".controls");
+buttons.addEventListener("click", function(event) {
+  if(event.target.tagName == "BUTTON") {
+    document.getElementById("")
+    console.log(event.target);
+  }
+})
+*/
