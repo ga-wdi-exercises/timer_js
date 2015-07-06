b16974e982eae6ff3c35aa1b5ae976a470a05b20
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-06 18:50:32 -0400

    drafted js

--- timers.js
+++ timers.js
@@ -1,4 +1,27 @@
+var resetButton = document.getElementById("reset");
+var startButton = document.getElementById("start");
+var pauseButton = document.getElementById("pause");
+var timerSpace = document.getElementById("timer");
 
+var count = 0;
+var countId;
 
+startButton.addEventListener("click", function() {
+    countId = setInterval(function (){
+    count++;
+    console.log(count);
+    timerSpace.innerHTML = "Time elapsed: " + count;
+    }, 1000);
+})
 
+resetButton.addEventListener("click", function() {
+  clearInterval(countId);
+  console.log(count);
+  timerSpace.innerHTML = "Stopwatch";
+  console.log("a click occurred on the reset button");
+})
 
+pauseButton.addEventListener("click", function() {
+  clearInterval(countId);
+  console.log("a click occurred on the pause button");
+})
