49a3de305b00944e5f6dea48e468a7ced521471e
Paul <paul.doulatshahi@gmail.com>
2015-07-06 19:32:32 -0400

    added pause/unpause and limited start button to one click

--- timers.js
+++ timers.js
@@ -6,6 +6,8 @@ var timerText = document.getElementById("timer")
 
 var seconds = 0
 var timerStart
+var countPause = 0
+var countStart = 0
 
 
 // 4. Create an `updateTime()` function that increments the `seconds` counter and inserts that value into the `<h1>` element with `id="timer"`.
@@ -18,9 +20,13 @@ function updateTime(){
 // Use setInterval() to increment the timer by 1 every second.
 startButton.addEventListener("click", function(){
   console.log("start");
+  countStart++;
+  countPause = 0;
+  if (countStart === 1)
 {
   timerStart = setInterval(updateTime, 1000);}
-})
+}
+)
 
 // Inside your click handler for the reset button...
 // Stop the timer using clearInterval().
@@ -31,6 +37,8 @@ resetButton.addEventListener("click", function(){
   clearInterval(timerStart);
   seconds = 0;
   timerText.innerHTML = "Stop Watch"
+  countPause = 0
+  countStart = 0;
 })
 
 // Inside your click handler for the pause button...
@@ -38,6 +46,13 @@ resetButton.addEventListener("click", function(){
 // Once again, inside your click handler for the start button...
 // Make sure the timer starts back up when you hit the "Start" button after hitting "Pause".
 pauseButton.addEventListener("click", function(){
+  countPause ++;
+  countStart = 0;
   console.log("pause");
-  clearInterval(timerStart);
-})
+  if (countPause % 2 != 0)
+  {
+  clearInterval(timerStart);}
+  else {
+    timerStart = setInterval(updateTime, 1000)}
+  }
+)
