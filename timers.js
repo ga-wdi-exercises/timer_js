11be99b671529deeea966cd785e50603f5c4e0cc
lanak1301 <musiconstellation@gmail.com>
2015-07-06 18:53:11 -0400

    timer start, pause, reset

--- timers.js
+++ timers.js
@@ -1,4 +1,42 @@
 
+var seconds = 0;
+var timerId = 0;
+
+var timer = document.getElementById( "timer" ) ;
+var reset = document.getElementById( "reset" ) ;
+var start = document.getElementById( "start" ) ;
+var pause = document.getElementById( "pause" ) ;
+
+
+start.addEventListener( "click", function() {
+	console.log("start timer") ;
+	timer.innerHTML = "Time elapsed: " + seconds ;
+	timerId = setInterval( updateTimer, 1000 ) ;
+})
+
+function updateTimer() {
+	seconds++ ;
+	timer.innerHTML = "Time elapsed: " + seconds ;
+} 
+
+
+pause.addEventListener( "click", function() {
+	clearInterval( timerId ) ;
+} )
+
+
+reset.addEventListener( "click", function() {
+	clearInterval( timerId ) ;
+	timer.innerHTML = "Stop watch" ;
+})
+
+
+
+
+
+
+
+
 
 
 
