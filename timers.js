cec1939c7a8d7154e9e7f99666c93543306953af
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-06 19:43:55 -0400

    trying the bonus. got start button to work

--- timers.js
+++ timers.js
@@ -1,3 +1,33 @@
+var timer = {
+  stopwatchInterval: 0,
+  stopwatchValue: 0,
+
+  buttons: {
+    timer: document.getElementById("timer"),
+    start: document.getElementById("start"),
+    pause: document.getElementById("pause"),
+    reset: document.getElementById("reset")
+  },
+
+  increaseStopwatch: function increaseStopwatch(){
+    this.stopwatchValue++;
+    this.buttons.timer.innerHTML = "Time elapsed: " + this.stopwatchValue;
+  },
+
+  startListeners: function startListeners(){
+    this.buttons.start.addEventListener("click", function(){
+      this.stopwatchInterval = setInterval(this.increaseStopwatch.bind(this), 1000);
+    }.bind(this));
+  }
+}
+
+timer.startListeners();
+
+
+
+
+/*
+This section works but wanted to try the bonus using one object timer{}
 var buttons = document.getElementById("allButtons");
 
 buttons.addEventListener("click", buttonClick);
@@ -23,4 +53,4 @@ var stopwatchValue = 0;
 function increaseStopwatch(){
   stopwatchValue ++;
   return document.getElementById("timer").innerHTML = "Time elapsed: " + stopwatchValue;
-}
+}*/
