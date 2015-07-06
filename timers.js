d2e043c3430238dcbaaea152b273b819b2f1ff0b
Matt Ficke <matthew.ficke@gmail.com>
2015-07-06 16:56:19 -0400

    declare initial variables, first DOM manipulation function

--- timers.js
+++ timers.js
@@ -1,4 +1,9 @@
+var reset = document.getElementById("reset");
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var timer = document.getElementById("timer");
+var stopwatch = window.setInterval(newWatch, 500);
 
-
-
-
+function newWatch() {
+  timer.innerHTML = "Time elapsed: 0";
+}
