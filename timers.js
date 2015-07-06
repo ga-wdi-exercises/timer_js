b9a0aba5e761906dfc272b6cfce1e19b582630c1
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-06 18:22:58 -0400

    initial commit. added eventlistener for div #buttons, changed text when start is clicked

--- timers.js
+++ timers.js
@@ -1,4 +1,10 @@
+var buttons = document.getElementById("allButtons");
 
+buttons.addEventListener("click", buttonClick);
 
+function buttonClick(){
+  if (event.target.id == "start"){
+    document.getElementById("timer").innerHTML = "Time Elapsed: 0";
+  }
 
-
+}
