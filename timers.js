6d97256865fd39a1b0836c4a60cf77ea2ec90d67
Edward Park <parkes.edward@gmail.com>
2015-07-06 19:29:38 -0400

    finished pause, start and reset

--- timers.js
+++ timers.js
@@ -1,12 +1,12 @@
 
-//var intervalID = window.setInterval(changeTimeOnStopWatch,1000);
 
 // Create Javascript selectors that target each of the timer buttons.
-
+var intervalID;
 var resetBtn = document.getElementById('reset');
 var startBtn = document.getElementById('start');
 var pauseBtn = document.getElementById('pause');
-
+var stopWatchTimer = document.getElementById('timer');
+var count = 0;
 // Create click handlers (empty, for now) for each of the timer buttons.
 
 resetBtn.addEventListener("click",resetClickEvent);
@@ -15,25 +15,30 @@ pauseBtn.addEventListener("click",pauseClickEvent);
 
 function resetClickEvent(){
   console.log("thisevent has fired!");
+  clearInterval(intervalID);
+  document.getElementById("timer").innerHTML = "Stop Watch";
+  count = 0;
 };
 
 function startClickEvent(){
   console.log("thisevent has fired!");
+  updateTime();
+
 };
 
 function pauseClickEvent(){
-  console.log("thisevent has fired!");
+  document.getElementById("timer").innerHTML ="Paused"
+  clearInterval(intervalID);
+
 };
-// Instantiate seconds and timerId variables for your timer. The latter will make more sense after reading up on setInterval().
-// Create an updateTime() function that increments the seconds counter and inserts that value into the <h1> element with id="timer".
-// Inside your click handler for the start button...
-// Replace "Stop Watch" in the HTML with the content of the seconds variable.
-// Use setInterval() to increment the timer by 1 every second.
-// Inside your click handler for the pause button...
-// Stop -- but do not reset! -- the timer using clearInterval().
-// Once again, inside your click handler for the start button...
-// Make sure the timer starts back up when you hit the "Start" button after hitting "Pause".
-// Inside your click handler for the reset button...
-// Stop the timer using clearInterval().
-// Reset the timer.
-// Replace the time in your HTML with the original "Stop Watch" text.
+function updateTime(){
+  intervalID = window.setInterval(timerCount,1000);
+};
+
+function timerCount(){
+  count++;
+  console.log(count);
+  document.getElementById("timer").innerHTML = count;
+};
+
+//use global variables to create a pause unpause blocker for that and start variable to
