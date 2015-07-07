95e549fca89e2b608fe8b0932bd26fad7a8d0346
Christine Lovett <clovett07@gmail.com>
2015-07-07 07:53:01 -0400

    Added js to timers.js

--- timers.js
+++ timers.js
@@ -1,4 +1,22 @@
+reset.addEventListener("click", function(event))
 
+start.addEventListener("click", updateTime)
 
+pause.addEventListener("click", clearInterval(theTimer))
 
-
+var updateTime = setInterval(function(){theTimer()}, 1000);
+  function theTimer(){
+    var startClock = new time();
+    timerContent = startClock.toTimeString();
+}
+  timer.innerHTML = seconds;
+  function startClock(){
+    setInterval(updateTime, 1000)
+  }
+  var timerId = document.getElementById('timer')
+  var seconds = 0
+  var timerContent = timer.innerHTML;
+}
+var reset = document.getElementById("reset")
+var start = document.getElementById("start")
+var pause = document.getElementById("pause")
