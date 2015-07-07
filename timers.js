9b81a7fb7bea9cbcaac59022c797673ee166f957
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-07-06 22:27:26 -0400

    Final timers.js

--- timers.js
+++ timers.js
@@ -1,4 +1,30 @@
+var reset = document.getElementById("reset");
+var start = document.getElementById("start");
+var pause = document.getElementById("pause");
+var seconds = 0;
+var timerId;
+var timer = document.getElementById('timer');
 
+start.addEventListener("click", function (){
+  console.log("Start")
+  timer.innerHTML = seconds
+  timerId = window.setInterval(updateTime, 1000);
+  // pause.timer starts back up
+})
 
+pause.addEventListener("click", function (){
+  console.log("Pause")
+  window.clearInterval(timerId)
+})
 
+reset.addEventListener("click", function (){
+  console.log("Reset")
+  window.clearInterval(timerId)
+  seconds = 0;
+  timer.innerHTML = "StopWatch"
+})
 
+function updateTime() {
+  seconds++
+  timer.innerHTML = seconds
+}
