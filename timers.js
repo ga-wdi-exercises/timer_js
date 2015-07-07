f07b873e48a6b83f2835958a6b2475a0dc57e7c5
Jeff Jones <jeff.jones1@gmail.com>
2015-07-06 20:49:05 -0400

    Working Timer

--- timers.js
+++ timers.js
@@ -3,7 +3,9 @@
 var reset = document.querySelector ('#reset')
 var start = document.querySelector('#start')
 var pause = document.querySelector('#pause')
+var timerContents = document.querySelector('#timer')
 
+var startTest = 1
 
 var timer = {
   seconds: 0,
@@ -11,31 +13,31 @@ var timer = {
 }
 function updateTime (){
     timer.seconds += 1
-    document.querySelector('#timer').innerHTML = timer.seconds;
-
-}
-
-function pauseTimer(){
-    clearInterval(timeAdd)
+    timerContents.innerHTML = timer.seconds;
 
 }
 
 // Reset Event
 
 reset.addEventListener ("click", function () {
+      clearInterval(timeAdd);
+      timerContents.innerHTML = 'Stop Watch';
 
 })
 
 // Start Event
 
 start.addEventListener ("click", function () {
-  if (document.querySelector('#timer').innerHTML === 'Stop Watch'){
-  timeAdd = setInterval(updateTime, 1000);
-  }
-})
+    if (startTest === 1) {
+      timeAdd = setInterval(updateTime, 1000)
+      startTest += 1
+    }
+  })
+
 
 // Pause Event
 
 pause. addEventListener ("click", function () {
-    pauseTimer();
+    clearInterval(timeAdd)
+    startTest = 1;
 })
