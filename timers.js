00abb0c3535f7788fe4e6928abbff78884dbe595
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-06 20:52:31 -0400

    first function and additional variables created

--- timers.js
+++ timers.js
@@ -1,3 +1,15 @@
-var b1 = getElementbyId(reset);
-var b2 = getElementbyId(start);
-var b3 = getElementbyId(pause);
+document.getElementbyId("reset").onclick = clearInterval();
+document.getElementbyId("start").onclick = setInterval();
+document.getElementbyId("pause").onclick = clearInterval();
+
+var s = seconds;
+var t = timerID;
+
+function updateTime() {
+  for (var i=0; i>0; i++) {
+   document.createElement("timer");
+  }
+}
+
+// window.setInterval();
+// window.clearInterval();
