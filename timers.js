92c817446f896ef093e2f231f11cf410ec703a9a
Mostafa Habib <MostafaSHabib@gmail.com>
2015-07-06 22:05:16 -0400

    everything working correctly

--- timers.js
+++ timers.js
@@ -1,4 +1,37 @@
+var reset = document.getElementById('reset')
+var start = document.getElementById('start')
+var pause = document.getElementById('pause')
+var timer = document.getElementById('timer')
+var secs = 0;
+var timerID;
+
+function timerCount() {
+  secs++
+  timer.innerHTML = secs
+};
 
 
+start.addEventListener('click', function() {
+  console.log('start');
+  timer.innerHTML = 'time ' +secs;
+  timerID = setInterval(timerCount, 1000)
+  });
+
+pause.addEventListener('click', function(){
+  console.log('pause');
+  clearInterval(timerID);
+});
 
 
+reset.addEventListener('click', function(){
+  console.log('reset');
+  start = 0;
+  clearInterval(timerID);
+  timer.innerHTML = 'stop'
+});
+
+
+//
+// function timerSetInterval(){
+//   timer.setInterval(timerCount, 5)
+// }
