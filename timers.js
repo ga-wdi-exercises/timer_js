9a5c25b541af40866a317fc76d71a63439af07de
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-06 22:34:57 -0400

    functions working as intended

--- timers.js
+++ timers.js
@@ -1,30 +1,24 @@
-var r = document.querySelector("reset");
-var s = document.querySelector("start");
-var p = document.querySelector("pause");
-var t = document.querySelector("timer"); //this id will change the stopwatch text
+var resetButton = document.querySelector("#reset");
+var startButton = document.querySelector("#start");
+var pauseButton = document.querySelector("#pause");
+var timerMorph = document.querySelector("#timer"); //this id will change the stopwatch text
 
 var seconds = 0;
 
-var b1 = "resetButton";
-var b2 = "startButton";
-var b3 = "pauseButton";
-
-resetButton.addEventListener("click, resetButtonChosen");
-startButton.addEventListener("click, startButtonChosen");
-pauseButton.addEventListener("click, pauseButtonChosen");
+resetButton.addEventListener("click", resetButtonChosen);
+startButton.addEventListener("click", startButtonChosen);
+pauseButton.addEventListener("click", pauseButtonChosen);
 
 function resetButtonChosen() {
-  if
-
-  } else if
-
-  function startButtonChosen(){
-
-  } else if
-
-  function pauseButtonChosen(){
-
+  console.log("Reset got clicked!");
   }
-}
+function startButtonChosen() {
+    console.log("Start got clicked!");
+  }
+function pauseButtonChosen() {
+    console.log("Pause got clicked!");
+  }
+
+
 // window.setInterval();
 // window.clearInterval();
