9e2ff37a07bbfda44e9d5d4ee1dd27366011d82f
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-07 07:28:37 -0400

    initial commit, the start button works

--- timers.js
+++ timers.js
@@ -1,4 +1,21 @@
+var seconds = 0;
+var timerId
 
+//Start
+startBtn = document.getElementById('start');
+startBtn.addEventListener("click", startWatch)
+function startWatch(){
+  var intervalID = setInterval(updateTime, 1000);
+}
+//Pause
+pauseBtn = document.getElementById('pause');
+pauseBtn.addEventListener("click", pauseWatch)
+//Reset
+resetBtn = document.getElementById('reset');
+resetBtn.addEventListener("click", resetWatch)
 
-
-
+//Create an updateTime() function that increments the seconds counter and inserts that value into the <h1> element with id="timer"
+function updateTime() {
+  seconds ++
+  document.getElementById("timer").innerHTML = ("Time elapsed: " + seconds)
+}
