6f3b3aabc7d30d09365ebcfa3ff74788e7b80c6d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-07-06 23:28:57 -0400

    homework

--- timers.js
+++ timers.js
@@ -1,4 +1,25 @@
+var btnReset = document.querySelector("reset")
+  button.addEventListener("click", updateTime() {
+      var timer = document.getElementById("timer");
+      document.body.innerHTML = document.body.innerHTML.replace('Stop Watch', seconds);
+    }
+
+  })
+var btnStart = document.querySelector("start")
+  button.addEventListener("click", function (){
 
 
+  }))
+var btnPause = document.querySelector("pause")
+  button.addEventListener("click", function(){
 
 
+  })
+
+var seconds
+var timerId
+
+function updateTime() {
+  var timer = document.getElementById("timer");
+  document.body.innerHTML = document.body.innerHTML.replace('Stop Watch', 'hi');
+}
