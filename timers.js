5edb385045f489b6b3d0425964c97e1faa4157fb
lanak1301 <musiconstellation@gmail.com>
2015-07-06 23:54:46 -0400

    homework done

--- timers.js
+++ timers.js
@@ -1,4 +1,3 @@
-
 var seconds = 0;
 var timerId = 0;
 
@@ -29,6 +28,51 @@ reset.addEventListener( "click", function() {
 	clearInterval( timerId ) ;
 	timer.innerHTML = "Stop watch" ;
 })
+
+
+
+/*
+object which is not working
+
+var timer = {										
+
+	seconds : 0 ,
+	timerId : 0 ,
+
+	ids : { 
+		timer : document.getElementById( "timer" ) ,
+		reset : document.getElementById( "reset" ) ,
+		start : document.getElementById( "start" ) ,
+		pause : document.getElementById( "pause" ) ,
+	} ,
+
+	updateTimer : function() {
+		seconds++ ;
+		timer.innerHTML = "Time elapsed: " + seconds ;
+	} ,
+
+	eventListeners : function () {
+
+		start.addEventListener( "click", function() {
+		console.log("start timer") ;
+		timer.innerHTML = "Time elapsed: " + seconds ;
+		timerId = setInterval( updateTimer, 1000 ) ;
+		} ) ,
+
+		pause.addEventListener( "click", function() {
+		clearInterval( timerId ) ;
+		} ) ,
+
+		reset.addEventListener( "click", function() {
+		clearInterval( timerId ) ;
+		timer.innerHTML = "Stop watch" ;
+		} )
+	}
+
+}
+*/
+
+
 
 
 
