f9213cf5cfa4a24b4e831cb0d47873bf935d68f6
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-07-07 08:34:43 -0400

    Timer attempt

--- timers.js
+++ timers.js
@@ -1,4 +1,33 @@
+// [reset, start, pause] btn selected
+var reset = document.getElementById('reset');
+var start = document.getElementById('start');
+var pause = document.getElementById('pause');
 
+// timer set to 0 which is where it begins/ resets to.
+var seconds = 0;
 
+// will call this function ever second and carry out code within.
+var timerID = setInterval(updateTime, 1000);
 
+// reset the timer when the reset btn is clicked
+reset.addEventListener('click', resetTimer)
+function resetTimer() {
+  document.getElementById('timer').innerHTML = "Stop Watch"
+}
 
+// start the timer when start btn is clicked
+start.addEventListener('click', startTimer)
+function startTimer() {
+
+}
+
+// pause the timer when pause btn is clicked
+pause.addEventListener('click', pauseTimer)
+function pauseTimer() {
+
+}
+
+// Update the timer
+function updateTime() {
+
+}
