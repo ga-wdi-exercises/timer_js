24518ff5d8949292735eace3bbc2aaf285ed052f
Robert <i.am.robert.benjamin@gmail.com>
2015-07-06 23:14:40 -0400

    Add timers.js and the bonus
    Honestly mostly recreated the bonus from the solution for my own
    purposes, the initial version I created had huge issues with the scope
    of the setInterval() function. Don’t count the bonus!

--- timers.js
+++ timers.js
@@ -1,4 +1,28 @@
+var seconds = 0;
+var timerId;
+var timer = document.getElementById("timer");
+var	start = document.getElementById("start");
+var	pause = document.getElementById("pause");
+var reset = document.getElementById("reset");
 
+start.addEventListener("click", function() {
+	timer.innerHTML = "Time elapsed: " + seconds;
+	timerId = setInterval("updateTime()", 1000);
+});
 
+pause.addEventListener("click", function() {
+	clearInterval(timerId);
+});
 
+reset.addEventListener("click", function () {
+	seconds = 0;
+	clearInterval(timerId);
+	timer.innerHTML = "Stop Watch";
+});
 
+function updateTime() {
+	seconds++;
+	timer.innerHTML = "Time elapsed: " + seconds;
+}
+
+/* */
