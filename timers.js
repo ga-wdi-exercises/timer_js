09a1596b68de71c4c992a3bb2327972a3bc0e3cb
lanak1301 <musiconstellation@gmail.com>
2015-07-07 00:40:50 -0400

    timer as object

--- timers.js
+++ timers.js
@@ -1,40 +1,7 @@
-var seconds = 0;
-var timerId = 0;
-
-var timer = document.getElementById( "timer" ) ;
-var reset = document.getElementById( "reset" ) ;
-var start = document.getElementById( "start" ) ;
-var pause = document.getElementById( "pause" ) ;
-
-
-start.addEventListener( "click", function() {
-	console.log("start timer") ;
-	timer.innerHTML = "Time elapsed: " + seconds ;
-	timerId = setInterval( updateTimer, 1000 ) ;
-})
-
-function updateTimer() {
-	seconds++ ;
-	timer.innerHTML = "Time elapsed: " + seconds ;
-} 
-
-
-pause.addEventListener( "click", function() {
-	clearInterval( timerId ) ;
-} )
-
-
-reset.addEventListener( "click", function() {
-	clearInterval( timerId ) ;
-	timer.innerHTML = "Stop watch" ;
-})
-
-
-
 /*
-object which is not working
-
-var timer = {										
+object which is working
+*/
+var watch = {										
 
 	seconds : 0 ,
 	timerId : 0 ,
@@ -47,32 +14,69 @@ var timer = {
 	} ,
 
 	updateTimer : function() {
-		seconds++ ;
-		timer.innerHTML = "Time elapsed: " + seconds ;
+		this.seconds++ ;
+		timer.innerHTML = "Time elapsed: " + this.seconds ;
 	} ,
 
 	eventListeners : function () {
+		var self = this;
 
 		start.addEventListener( "click", function() {
-		console.log("start timer") ;
-		timer.innerHTML = "Time elapsed: " + seconds ;
-		timerId = setInterval( updateTimer, 1000 ) ;
+			console.log("start timer") ;
+			timer.innerHTML = "Time elapsed: " + self.seconds ;
+			updateTimer = self.updateTimer.bind(self);
+			timerId = setInterval( updateTimer, 1000 ) ;
 		} ) ,
 
 		pause.addEventListener( "click", function() {
-		clearInterval( timerId ) ;
+			clearInterval( timerId ) ;
 		} ) ,
 
 		reset.addEventListener( "click", function() {
-		clearInterval( timerId ) ;
-		timer.innerHTML = "Stop watch" ;
+			clearInterval( timerId ) ;
+			timer.innerHTML = "Stop watch" ;
+			self.seconds = 0;
 		} )
 	}
 
 }
-*/
+
+watch.eventListeners();
 
 
+
+
+
+// var seconds = 0;
+// var timerId = 0;
+
+// var timer = document.getElementById( "timer" ) ;
+// var reset = document.getElementById( "reset" ) ;
+// var start = document.getElementById( "start" ) ;
+// var pause = document.getElementById( "pause" ) ;
+
+
+// start.addEventListener( "click", function() {
+// 	console.log("start timer") ;
+// 	timer.innerHTML = "Time elapsed: " + seconds ;
+// 	timerId = setInterval( updateTimer, 1000 ) ;
+// })
+
+// function updateTimer() {
+// 	seconds++ ;
+// 	timer.innerHTML = "Time elapsed: " + seconds ;
+// } 
+
+
+// pause.addEventListener( "click", function() {
+// 	clearInterval( timerId ) ;
+// } )
+
+
+// reset.addEventListener( "click", function() {
+// 	clearInterval( timerId ) ;
+// 	timer.innerHTML = "Stop watch" ;
+// })
 
 
 
