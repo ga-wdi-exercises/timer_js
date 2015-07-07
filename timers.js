20eef786b4e4b5a6bc0bf9b9870642f06370e062
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-07-07 08:57:07 -0400

    finished homework

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+console.log("script linked!");
 
+var reset = document.getElementById("reset")
+var start = document.getElementById("start")
+var pause = document.getElementById("pause")
 
+var second = 0
+var timerId;
 
+  reset.addEventListener("click",function(){
+    timer.innerHTML = second;
+    timerId = clearInterval()
+  })
+  start.addEventListener("click",function(){
+    timer.innerHTML = second;
+    timerId = setInterval(updateTime, 1000)
+    console.log("you clicked start")
+  })
+  pause.addEventListener("click",function(){
+    clearInterval(timerId)
+  })
 
+function updateTime(){
+  second++;
+timer.innerHTML = second;
+}
+
+var timer = document.getElementById("timer")
+document.timerId
