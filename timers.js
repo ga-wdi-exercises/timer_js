01b6a045d4a65213dc6076164ed354c1105d20c5
Yvonne Wang <yvonnewang22@gmail.com>
2015-07-07 06:07:04 -0400

    first commit

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+//Create selectors targeting each button
+var reset = document.querySelector ('#reset');
+var start = document.querySelector ('#start');
+var pause = document.querySelector ('#pause');
 
+//Create click handlers (empty, for now) for each of the timer buttons.
+reset.onclick = function (){
 
+}
 
+start.onclick = function (){
 
+}
+
+pause.onclick = function (){
+
+}
+
+//Instantiate seconds and timerId variables for your timer.
+// The latter will make more sense after reading up on setInterval().
+setInterval(function () {
+  var seconds;
+  var timerId;
+}
+)
+
+//Inside your click handler for the start button...
+//Replace "Stop Watch" in the HTML with the content of the seconds variable.
+//Use setInterval() to increment the timer by 1 every second.
