f7924905a0a3521723ee612ad8cd9fda4f55c478
Jeff Jones <jeff.jones1@gmail.com>
2015-07-06 20:31:04 -0400

    Mid point of itmer exercise

--- timers.js
+++ timers.js
@@ -1,4 +1,41 @@
+// Button vars
+
+var reset = document.querySelector ('#reset')
+var start = document.querySelector('#start')
+var pause = document.querySelector('#pause')
 
 
+var timer = {
+  seconds: 0,
+  timerID: 0,
+}
+function updateTime (){
+    timer.seconds += 1
+    document.querySelector('#timer').innerHTML = timer.seconds;
 
+}
 
+function pauseTimer(){
+    clearInterval(timeAdd)
+
+}
+
+// Reset Event
+
+reset.addEventListener ("click", function () {
+
+})
+
+// Start Event
+
+start.addEventListener ("click", function () {
+  if (document.querySelector('#timer').innerHTML === 'Stop Watch'){
+  timeAdd = setInterval(updateTime, 1000);
+  }
+})
+
+// Pause Event
+
+pause. addEventListener ("click", function () {
+    pauseTimer();
+})
