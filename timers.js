8e5b89622cacdae9450e581340e94ae963444b03
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-06 22:13:45 -0400

    additional variables added

--- timers.js
+++ timers.js
@@ -1,21 +1,30 @@
-var b1 = getElementbyId("reset");
-var b2 = getElementbyId("start");
-var b3 = getElementbyId("pause");
-var b4 = getElementbyId("timer"); //this id will change the stopwatch text
+var r = document.querySelector("reset");
+var s = document.querySelector("start");
+var p = document.querySelector("pause");
+var t = document.querySelector("timer"); //this id will change the stopwatch text
 
 var seconds = 0;
 
-function resetButtonChosen(){
+var b1 = "resetButton";
+var b2 = "startButton";
+var b3 = "pauseButton";
 
-};
+resetButton.addEventListener("click, resetButtonChosen");
+startButton.addEventListener("click, startButtonChosen");
+pauseButton.addEventListener("click, pauseButtonChosen");
 
-function startButtonChosen(){
+function resetButtonChosen() {
+  if
 
-};
+  } else if
 
-function pauseButtonChosen(){
+  function startButtonChosen(){
 
+  } else if
+
+  function pauseButtonChosen(){
+
+  }
 }
-
 // window.setInterval();
 // window.clearInterval();
