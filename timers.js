92933b8fae872a3d009e7efc6d4996d4f268e67e
Nicholas Olds <nolds9@yahoo.com>
2015-07-07 00:11:19 -0400

    Adding completed basic hw

--- timers.js
+++ timers.js
@@ -1,4 +1,46 @@
+var seconds = 0;
 
+var clear = document.getElementById("reset")
+var go = document.getElementById("start");
+var pause  = document.getElementById("pause");
+var t = document.getElementById("timer");
 
+var newT;
+var started = false;
 
+// Button listeners
+clear.addEventListener("click", clearT);
+go.addEventListener("click", goT);
+pause.addEventListener("click", pauseT);
 
+// Clear Timer
+function clearT() {
+  clearInterval(newT);
+  seconds = 0;
+  t.innerHTML = "Stop Watch";
+  started = false;
+
+}
+
+//Start Timer
+function goT() {
+  if (!started){
+    t.innerHTML = "Time elapsed: " + seconds + " seconds";
+    newT = setInterval(timeDisplay, 1000);
+    started = true;
+    console.log("Started timer")
+  }
+}
+
+//Pause Timer
+function pauseT() {
+  clearInterval(newT);
+  started = false;
+  console.log("Paused Timer");
+
+}
+// Time Displayed
+function timeDisplay() {
+  seconds += 1;
+  t.innerHTML = "Time elapsed: " + seconds + " seconds";
+}
