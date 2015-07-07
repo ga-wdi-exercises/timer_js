7e8c7aa5f9693ba4901de4591ade260cfeaad3b1
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-07 08:24:32 -0400

    added functionality to pause and reset buttons

--- timers.js
+++ timers.js
@@ -1,18 +1,27 @@
 var seconds = 0;
-var timerId
+var timerId;
 
 //Start
 startBtn = document.getElementById('start');
 startBtn.addEventListener("click", startWatch)
 function startWatch(){
-  var intervalID = setInterval(updateTime, 1000);
+  timerId = setInterval(updateTime, 1000);
 }
 //Pause
 pauseBtn = document.getElementById('pause');
 pauseBtn.addEventListener("click", pauseWatch)
+function pauseWatch(){
+  //Stop -- but do not reset! -- the timer using clearInterval()
+  clearInterval(timerId);
+}
 //Reset
 resetBtn = document.getElementById('reset');
 resetBtn.addEventListener("click", resetWatch)
+function resetWatch(){
+  clearInterval(timerId);
+  seconds = 0;
+  document.getElementById("timer").innerHTML = "Stop Watch";
+}
 
 //Create an updateTime() function that increments the seconds counter and inserts that value into the <h1> element with id="timer"
 function updateTime() {
