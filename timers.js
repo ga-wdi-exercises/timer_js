03afb3f53567e9392ab4093189641c739bbd3d7a
Robert <i.am.robert.benjamin@gmail.com>
2015-07-07 23:37:52 -0400

    Refactor code to streamline
    Now using only one Event Listener! It’s much cleaner code, and works
    just as well as before.

--- timers.js
+++ timers.js
@@ -1,28 +1,26 @@
 var seconds = 0;
 var timerId;
-var timer = document.getElementById("timer");
-var	start = document.getElementById("start");
-var	pause = document.getElementById("pause");
-var reset = document.getElementById("reset");
+var controls = document.querySelector(".controls");
+controls.addEventListener("click", runControls, false);
 
-start.addEventListener("click", function() {
-	timer.innerHTML = "Time elapsed: " + seconds;
-	timerId = setInterval(updateTime, 1000);
-});
-
-pause.addEventListener("click", function() {
-	clearInterval(timerId);
-});
-
-reset.addEventListener("click", function () {
-	seconds = 0;
-	clearInterval(timerId);
-	timer.innerHTML = "Stop Watch";
-});
+function runControls(e) {
+	if(e.target !== e.currentTarget) {
+		var clickedItem = e.target.id
+		if(clickedItem === 'start') {
+			timer.innerHTML = "Time elapsed: " + seconds;
+			timerId = setInterval(updateTime, 1);
+		} else if (clickedItem === 'pause') {
+			clearInterval(timerId);
+		} else if (clickedItem === 'reset') {
+			seconds = 0;
+			clearInterval(timerId);
+			timer.innerHTML = "Stop Watch";
+		}
+	}
+	e.stopPropagation();
+}
 
 function updateTime() {
 	seconds++;
 	timer.innerHTML = "Time elapsed: " + seconds;
 }
-
-/* */
