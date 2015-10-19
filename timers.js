68e69511294a920ccd837b8b8b58be53216f6f23
Matt Popovich <mattpopovich@outlook.com>
2015-10-19 14:33:07 -0400

    Started some stuff

--- timers.js
+++ timers.js
@@ -1,4 +1,22 @@
 
+getElementById("reset").addEventListener("click", function(){
 
+}, false);
 
+getElementById("start").addEventListener(
+  "click",
+  function(){
+    setInterval(function (){
+        console.log("ONE SECOND HAS PASSED!");
+      }, 1000);
+  }, false
+);
 
+getElementById("pause").addEventListener(
+  "click",
+  function(){
+    setInterval(function (){
+        console.log("ONE SECOND HAS PASSED!");
+      }, 0);
+  }, false
+);
