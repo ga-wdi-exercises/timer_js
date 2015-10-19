ba8c0b0bdbde17d0c76a7549e91e5354be1cf432
Davis Green <davismgreen@gmail.com>
2015-10-19 19:26:26 -0400

    Initial Commit; Complete Homework, bonus needs work

--- timers.js
+++ timers.js
@@ -1,4 +1,34 @@
+var timer = {
 
+  addListeners: function() {
+    var seconds = 0;
+    var timerID = 0;
+    var updateTime = function() {
+        seconds = seconds + 1
+        document.getElementById("timer").innerHTML = seconds;
+      };
 
+    var startClick = function() {
+      if (seconds === 0){
+        document.getElementById("timer").innerHTML=0;
+      }
+      clearInterval(timerID)
+      timerID = setInterval(updateTime,1000)
+    };
 
+    var pauseClick = function() {
+      clearInterval(timerID)
+    };
 
+    var resetClick = function() {
+        clearInterval(timerID);
+        seconds = 0
+        document.getElementById("timer").innerHTML = "Stop Watch"
+      };
+
+    document.getElementById("start").addEventListener("click", startClick);
+    document.getElementById('pause').addEventListener("click", pauseClick);
+    document.getElementById('reset').addEventListener("click", resetClick);
+  }(),
+
+}
