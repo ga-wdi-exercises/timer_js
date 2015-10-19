0bc4099eed36f6927d474770593476aeeb3db35b
jen pengelly <jgpengelly@gmail.com>
2015-10-19 17:31:09 -0400

    timers.js: initial setup of variables and clickEventListeners

--- timers.js
+++ timers.js
@@ -1,4 +1,22 @@
+var resetButton = document.querySelector("#reset")
+var startButton = document.querySelector("#start")
+var pauseButton = document.querySelector("#pause")
 
+resetButton.addEventListener("click",)
+startButton.addEventListener("click",)
+pauseButton.addEventListener("click",)
 
+//  Seconds variable for timer
+function seconds (){
+  console.log("This will show seconds?");
+};
+setInterval(seconds, 1000);
 
-
+// timerID variable
+var timerId;
+pause.addEventListener("click", function(){
+  timerID = setInterval(, 1000);
+});
+stop.addEventListener("click", function(){
+  clearInterval(timerId);
+});
