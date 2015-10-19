94a5d59784233eeed05bdbcebb6937c72fbee3be
Chase Gruber <chasegruber@gmail.com>
2015-10-19 14:35:49 -0400

    working timer object

--- timers.js
+++ timers.js
@@ -1,26 +1,59 @@
-/*var timer = {
+var timeDisplay = document.getElementById('timer');
+var startBtn = document.getElementById('start');
+var pauseBtn = document.getElementById('pause');
+var resetBtn = document.getElementById('reset');
 
-}*/
-var tick;
-var timer = document.getElementById('timer');
-var reset = document.getElementById('reset');
-var start = document.getElementById('start');
-var pause = document.getElementById('pause');
+var timer = {
+  // setup:function(){
+  //   timeDisplay = document.getElementById('timer');
+  //   var startBtn = document.getElementById('start');
+  //   var pauseBtn = document.getElementById('pause');
+  //   var resetBtn = document.getElementById('reset');
+  //   startBtn.addEventListener('click',function(){
+  //     return this.startTimer();
+  //   }.bind(this));
+  //   pauseBtn.addEventListener('click',function(){
+  //     return this.pauseTimer();
+  //   }.bind(this));
+  //   resetBtn.addEventListener('click',function(){
+  //     return this.resetTimer();
+  //   }.bind(this));
+  // },
+  seconds:0,
+  started: false,
+  //added this ^ property to prevent extra 'start' clicks from creating interval inside interval;
+  startTimer: function(){
+    if (this.started === false){
+      this.updateTimer();
+      this.started = true;
+    }
+  },
+  updateTimer: function(){
+    this.incrementTimer();
+    tick = setInterval(this.incrementTimer.bind(this),1000);
+  },
+  incrementTimer: function(){
+    timeDisplay.textContent = "Time Elapsed: "+this.seconds;
+    this.seconds++;
+  },
+  pauseTimer: function(){
+    if (this.started === true){
+      clearInterval(tick);
+      this.started = false;
+    }
+  },
+  resetTimer: function(){
+    this.seconds = 0;
+    timeDisplay.textContent = "Time Elapsed: "+this.seconds;
+  }
+}
 
-var update = function(){
-  tick = setInterval(increment,1000);
-}
-var time = 0;
-function increment(){
-  timer.textContent = "Time Elapsed: "+time;
-  time++;
-}
-
-start.onclick = update;
-pause.onclick = function(){
-  clearInterval(tick);
-}
-reset.onclick = function(){
-  time = 0;
-  timer.textContent = "Time Elapsed: "+time;
-}
+startBtn.addEventListener('click',function(){
+  return timer.startTimer();
+});
+pauseBtn.addEventListener('click',function(){
+  return timer.pauseTimer();
+});
+resetBtn.addEventListener('click',function(){
+  return timer.resetTimer();
+});
