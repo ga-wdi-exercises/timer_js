df12efb943455756532fec194dc763368ba12e5b
Chase Gruber <chasegruber@gmail.com>
2015-10-19 10:21:05 -0400

    working, ugly timer

--- timers.js
+++ timers.js
@@ -1,4 +1,26 @@
+/*var timer = {
 
+}*/
+var tick;
+var timer = document.getElementById('timer');
+var reset = document.getElementById('reset');
+var start = document.getElementById('start');
+var pause = document.getElementById('pause');
 
+var update = function(){
+  tick = setInterval(increment,1000);
+}
+var time = 0;
+function increment(){
+  timer.textContent = "Time Elapsed: "+time;
+  time++;
+}
 
-
+start.onclick = update;
+pause.onclick = function(){
+  clearInterval(tick);
+}
+reset.onclick = function(){
+  time = 0;
+  timer.textContent = "Time Elapsed: "+time;
+}
