d90ba6c0c8212fbf46fc605ff894c5d6f1d052b3
Elise Kain <elise.kain@gmail.com>
2015-10-19 14:03:39 -0400

    Make all initial stopwatch functions work

--- timers.js
+++ timers.js
@@ -1,4 +1,39 @@
+// Get All DOM Elements
+var reset = document.getElementById("reset");
+var start = document.getElementById("start");
+var pause = document.getElementById("pause");
+var timer = document.getElementById("timer");
 
+// Initialize Timer at 0
+var seconds = 0;
 
+// Initialize timerID
+var timerID;
 
+// Update Time Function
+var updateTime = function () {
+  seconds++;
+  timer.textContent = seconds;
+}
 
+//Reset Button Listener
+reset.addEventListener("click", function() {
+  // Pause timer and reset seconds to 0
+  clearInterval(timerID);
+  seconds = 0;
+  // Replace timer text with "Stop Watch"
+  timer.textContent = "Stop Watch";
+});
+
+// Start Button Listener
+start.addEventListener("click", function(){
+  // Set Interval at which to run updateTime Function
+  timerID = setInterval(updateTime, 1000);
+  // Replace "Stop Watch" with timer
+  timer.textContent = seconds;
+});
+
+// Pause Button Listener
+pause.addEventListener("click", function(){
+  clearInterval(timerID);
+});
