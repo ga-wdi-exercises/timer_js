66d4f2e89070b43d0127e8485a5d40de7add8bcd
Josh Cohen <jacohen10@EMAIL.COM>
2015-10-19 18:56:05 -0400

    initial commit

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+var startID = document.querySelector("#start");
+var pauseID = document.querySelector("#pause");
+var resetID = document.querySelector("#reset");
+var timerID = document.querySelector("#timer");
+var seconds = 0;
 
 
+var updateTime = function () {
+  seconds++;
+  timerID.textContent = "Timer:" + seconds;
+};
+
+var start;
+startID.addEventListener("click", function(){
+  start = setInterval(updateTime, 1000);
+});
 
 
+var pause;
+pauseID.addEventListener("click", function(){
+  clearInterval(start);
+});
+
+var reset;
+resetID.addEventListener("click", function(){
+  clearInterval(start);
+  timerID.textContent = "Stop Clock";
+  seconds = 0;
+});
