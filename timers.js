8478f4957dc4a8cced5ed39efcb621ef8c8011e4
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-20 12:17:17 -0400

    Finished timer javascript, not inside an object yet.

--- timers.js
+++ timers.js
@@ -1,4 +1,33 @@
+var resetButton = document.getElementById("reset")
+var startButton = document.getElementById("start")
+var pauseButton = document.getElementById("pause")
 
+resetButton.addEventListener("click",resetButtonClick)
+startButton.addEventListener("click",startButtonClick)
+pauseButton.addEventListener("click",pauseButtonClick)
 
+var seconds = 0
+var timerId
+var h1Tag = document.getElementById('timer');
 
+function updateTime(){
+  seconds = seconds+1
+  h1Tag.textContent = seconds
+}
 
+function resetButtonClick(){
+  startButton.addEventListener("click",startButtonClick)
+  timerId = window.clearInterval(timerId)
+  seconds = 0;
+  h1Tag.textContent = "Stop Watch"
+}
+
+function startButtonClick(){
+  startButton.removeEventListener("click",startButtonClick)
+  timerId = window.setInterval(updateTime,1000)
+  //add a remove event listener here to avoid adding multiple events of the stopwatch starting
+}
+function pauseButtonClick(){
+  startButton.addEventListener("click",startButtonClick)
+  timerId = window.clearInterval(timerId)
+}
