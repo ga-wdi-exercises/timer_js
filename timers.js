a1628920c0c48ebfe0a4d80c489fff5e703f071f
jen pengelly <jgpengelly@gmail.com>
2015-10-20 17:54:22 -0400

    update psuedo-code to detail client requirements

--- timers.js
+++ timers.js
@@ -1,18 +1,42 @@
+// Create JS selectors to target each of the timer buttons
 var resetButton = document.querySelector("#reset")
 var startButton = document.querySelector("#start")
 var pauseButton = document.querySelector("#pause")
 
+// create click handlers for the timer buttons
 resetButton.addEventListener("click",)
 startButton.addEventListener("click",)
 pauseButton.addEventListener("click",)
 
-//  Seconds variable for timer
+//  Instantiate seconds and timerId veriables?
+var seconds = 0;
+var timerId = 0;
+
+
+// Start Functionality
+  // click start
+    //  replace text "Stop Watch" with "Time elapsed: 0"
+    // increase count by 1 second.
+
+
+// Reset Functionality
+  // click reset
+    //text returns to Stop Watch
+
+// Pause Functionality
+  // click pause
+    // text stays at time elapsed
+    // stops incrementing
+
+
+
+// setInterval alt?
 function seconds (){
   console.log("This will show seconds?");
 };
 setInterval(seconds, 1000);
 
-// timerID variable
+// timerID variable alt?
 var timerId;
 pause.addEventListener("click", function(){
   timerID = setInterval(, 1000);
