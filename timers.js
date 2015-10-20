d6f2723afd9e887515483b3f519fd769cbec4812
James Moon <mooniker@gmail.com>
2015-10-20 18:57:11 -0400

    Implements stop watch with Javascript
    Uses a global variable to keep of the timer's state to avoid multiple click
    events for the start button.

--- timers.js
+++ timers.js
@@ -4,25 +4,34 @@ var start = document.querySelector("#start");
 var pause = document.querySelector("#pause");
 var reset = document.querySelector("#reset");
 
+var timer_is_running = false; // Keeps track of whether the timer is running.
+
 // Create click handlers for each of the timer buttons.
 
 start.addEventListener("click", function () {
   // start
+  if ( timer_is_running ) {
+    console.log("Timer already started.");
+  } else {
   console.log("Timer started.");
-  // Replace "Stop Watch" in the HTML with the content of the seconds variable.
-  document.querySelector("#timer").innerText = "Time elapsed: " + seconds;
-  // Use setInterval() to increment the timer by 1 every second
-  timerId = setInterval(updateTime, 1000);
-  // Make sure the timer starts back up when you hit the "Start" button after hitting "Pause".
-  this.removeEventListener("click");
-
+    // Replace "Stop Watch" in the HTML with the content of the seconds variable.
+    document.querySelector("#timer").innerText = "Time elapsed: " + seconds;
+    // Use setInterval() to increment the timer by 1 every second
+    timerId = setInterval(updateTime, 1000);
+    timer_is_running = true;
+  }
 });
 
 pause.addEventListener("click", function () {
   // pause
-  console.log("Timer paused.");
-  // Stop -- but do not reset! -- the timer using clearInterval().
-  clearInterval(timerId);
+  if ( timer_is_running ) {
+    console.log("Timer paused.");
+    // Stop -- but do not reset! -- the timer using clearInterval().
+    clearInterval(timerId);
+    timer_is_running = false;
+  } else {
+    console.log("Timer already paused.");
+  }
 });
 
 reset.addEventListener("click", function () {
@@ -34,6 +43,7 @@ reset.addEventListener("click", function () {
   seconds = 0;
   // Replace the time in your HTML with the original "Stop Watch" text.
   document.querySelector("#timer").innerText = "Stop Watch";
+  timer_is_running = false;
 });
 
 // Instantiate seconds and timerId variables for the timer.
