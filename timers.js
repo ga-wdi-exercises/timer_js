dd7b4003fb2bbef3f130374d07cd5633193376d6
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-10-20 19:16:55 -0400

    Timer Homework, Grodsky

--- timers.js
+++ timers.js
@@ -1,3 +1,43 @@
+// addListeners: function () {
+
+  var seconds = 0
+  var timerId //realized i don't need to set this to anything, as its just a palceholder name
+
+  var updateTime = function() {
+    seconds++
+    document.getElementById("timer").innerHTML = seconds;
+  }
+
+//StartButton is teh first time we are seeing timerId, so integer is getting stored in this variable
+var startButton = function() {
+  if (seconds === 0) {
+    document.getElementById("timer").innerHTML = 0;
+  }
+  clearInterval(timerId)
+  timerId = setInterval(updateTime, 1000)
+};
+
+//had so much trouble getting pause to work. ended up being more simple than i thought. Relied on console.log to test different methods
+var pauseButton = function() {
+  clearInterval(timerId)
+  console.log("AM i paused yet?")
+};
+
+var resetButton = function() {
+  clearInterval(timerId)
+  seconds = 0
+  document.getElementById('timer').innerHTML = "Stop Watch"
+};
+
+
+document.getElementById("start").addEventListener("click", startButton);
+document.getElementById("pause").addEventListener("click", pauseButton);
+document.getElementById("reset").addEventListener("click", resetButton);
+
+
+
+//Here is all the steps i went through in order to get the above code!
+
 // //create javascript selectors that target each of the timer buttons
 // var resetButton  = document.querySelector("button");
 // var handleClickEvent = function (){
@@ -14,43 +54,30 @@
 
 
 //#3 something later to initiate - global variables
+// var button = document.getElementById("start");
+// var handleClickEvent = function() {
+//   document.getElementById('reset')
+//   var setInterval = function() {
+//     //use timerID
+//   }
+// // }
+//
+// var button = document.getElementById("reset");
+// var handleClickEvent = function() {
+//     console.log("I have been clicked")
+//   }
+//   button.addEventListener("click", handleClickEvent)
 
-var seconds = 0
-var timerId = 0
-
-var updateTime = function (){
-seconds = seconds + 1
-document.getElementById('timer').innerHTML = seconds;
-}
-
-
-var button = document.getElementById("reset");
-var handleClickEvent = function(){
-  console.log("I have been clicked")
-}
-button.addEventListener("click", handleClickEvent)
-
-//#5 - replace stop watch in the html with the content of teh seconds variable. Use setInterval to increment the timer by 1 every second
-var button = document.getElementById("start");
-var handleClickEvent = function(){
-  console.log("I have also been clicked")
-var setInterval = function (){
-//use timerID
-  }
-}
-button.addEventListener("click", handleClickEvent)
-
-
-
-
-var button = document.getElementById("pause");
-var handleClickEvent = function(){
-  console.log("Don't forget about me being clicked!!")
-}
-button.addEventListener("click", handleClickEvent)
-
+//
+// //#5 - replace stop watch in the html with the content of teh seconds variable. Use setInterval to increment the timer by 1 every second
+//
+//
+// var button = document.getElementById("pause");
+// var handleClickEvent = function() {
+//   console.log("Don't forget about me being clicked!!")
+// }
+// button.addEventListener("click", handleClickEvent)
 
 
 //take seconds var from up top, u
 // insert this value into teh h1 element with id = timer
-};
