3c782d177f88bff87b2ab6b2c4b9a66a9c3a067d
Samir Mehta <sammehta88@gmail.com>
2015-10-20 19:38:59 -0400

    Add stopwatch

--- timers.js
+++ timers.js
@@ -2,6 +2,8 @@ var startButton = document.getElementById('start');
 var resetButton = document.getElementById('reset');
 var pauseButton = document.getElementById('pause');
 var timerText = document.getElementById('timer');
+var bar = document.getElementById('bar');
+var level = document.querySelector('.level');
 
 var timer = {
   seconds: 0,
@@ -89,105 +91,48 @@ var timer = {
   }
 };
 
-timer.init();
+var stopwatch = {
+  init: function() {
+    timerText.innerHTML = 'Stopwatch';
+    level.style.backgroundColor = 'rgba(0,176,0,1.0)';
+    this.getInitialSeconds();
+    this.seconds = this.initialSeconds;
+  },
+  getInitialSeconds: function() {
+    this.initialSeconds = parseInt(prompt('How long?'));
+  },
+  addClickListeners: function() {
+    var self = this;
+    startButton.addEventListener('click', function() {
+      self.init();
+      self.updateTime();
+    });
+  },
+  updateTime: function(){
+    var self = this;
+    self.timerId = window.setInterval(function(){
+        self.seconds -= 1;
+        timerText.innerHTML = 'Seconds Left = ' + self.seconds;
+        self.updateLevel();
+      }, 1000);
+  },
+  updateLevel: function(){
+    var self = this;
+    self.percent = self.seconds / self.initialSeconds * 100;
+    level.style.width = self.percent + '%';
 
-// var seconds = 0;
-// var timerId;
-// var red, green, blue, alpha, goingUp;
-// red = 0;
-// green = 0;
-// blue = 0;
-// alpha = 0.125;
-// goingUp = true;
-// var start = false;
+    if (self.seconds === 0){
+      self.stopTimer(self.timerId);
+    } else if (self.percent <= 10) {
+      level.style.backgroundColor = 'red';
+    } else if (self.percent <= 50) {
+      level.style.backgroundColor = 'yellow';
+    }
+  },
+  stopTimer: function(id) {
+    clearInterval(id);
+    this.start = false;
+  }
+};
 
-// var startButton = document.getElementById('start');
-// var resetButton = document.getElementById('reset');
-// var pauseButton = document.getElementById('pause');
-// var timerText = document.getElementById('timer');
-//
-// var buttonArray = [startButton, resetButton, pauseButton];
-//
-// startButton.addEventListener('click', function( event ) {
-//   if(!start) {
-//     updateTime();
-//     start = true;
-//   }
-// });
-//
-// resetButton.addEventListener('click', function( event ) {
-//   stopTimer(timerId);
-//   seconds = 0;
-//   timerText.innerHTML = 'Stop Watch';
-//   document.body.style.backgroundColor = 'white';
-// });
-//
-// pauseButton.addEventListener('click', function( event ) {
-//   stopTimer(timerId);
-// });
-//
-// buttonArray.forEach(function(button){
-//   button.addEventListener('click', function(){
-//     clearClicked();
-//     toggleClicked(this);
-//   });
-// });
-
-// function updateTime() {
-//   //window or document.setIneterbal?
-//   // if (!timerId) {
-//     timerId = window.setInterval(function(){
-//       seconds += 1;
-//       timerText.innerHTML = 'Time Elapsed = ' + seconds;
-//       updateColor();
-//     }, 1000);
-//   // }
-// }
-//
-// function stopTimer(id) {
-//   clearInterval(id);
-//   start = false;
-// }
-
-// function updateColor() {
-//   var bod = document.body;
-//   red = randomNumber() * 32;
-//   green = randomNumber() * 32;
-//   blue = randomNumber() * 32;
-//   alpha = Math.random();
-//
-//   while (alpha < 0.1) {
-//     alpha = Math.random();
-//   }
-//
-//   // if (goingUp && alpha <= 1.0) {
-//   //   alpha += 0.125;
-//   // } else if (alpha > 1.0) {
-//   //   alpha = 1.0;
-//   //   goingUp = false;
-//   // } else if (!goingUp && alpha > 0) {
-//   //   alpha -= 0.125;
-//   // } else {
-//   //   goingUp = true;
-//   // }
-//     // red = randomNumber() * 32;
-//     // green = randomNumber() * 32;
-//     // blue = randomNumber() * 32;
-//
-//   bod.style.backgroundColor = 'rgba(' + red + ',' + green + ',' + blue + ',' + alpha + ')';
-//   // bod.style.backgroundColor = 'rgba(27,133,61,' + alpha + ')';
-// }
-//
-// function randomNumber() {
-//   return Math.floor(Math.random() * 9);
-// }
-
-// function toggleClicked( element ) {
-//   element.className = 'clicked';
-// }
-//
-// function clearClicked() {
-//   for (var i = 0; i < buttonArray.length; i++) {
-//     buttonArray[i].className = '';
-//   }
-// }
+stopwatch.addClickListeners();
