9a32cce1b627d2089b4fa319e1840af2b63040f4
Ly Nguyen <Ly900@aol.com>
2015-10-19 23:50:07 -0400

    finish pause button (#6-7)

---
+++ jstestfile.js
@@ -0,0 +1,4 @@
+var resetButton = document.querySelector("#reset");
+var startButton = document.querySelector("#start");
+var pauseButton = document.querySelector("#pause");
+var h1 = document.querySelector("#timer");
