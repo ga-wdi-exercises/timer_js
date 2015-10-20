35f3e1c77c2893e2fd004cd94e4ec8de4dddb6da
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-10-20 19:27:40 -0400

    Save changes

--- timers.js
+++ timers.js
@@ -1,87 +1,100 @@
-// THIS CODE WORKS BUT ITS NOT WELL WRITTEN!
 var timer = {
   start:  document.getElementById('start'),
   reset:  document.getElementById('reset'),
   stop:   document.getElementById('start'),
   pause:  document.getElementById('pause'),
-  seconds:  0,
+  timerElement:  document.getElementById('timer'),
+  seconds: 0,
+  timerID: 0,
+
   changeName: function(){
     this.start.textContent = "Stop";
   },
   changeBack: function (){
     this.start.textContent = "Start";
   },
-  clickStart: function (){timer.start.addEventListener("click", function(){
-          if (timer.seconds <= 0 || timer.start.textContent == "Start"){
-            timerID = setInterval(function(){
-            timer.start.setAttribute("id", timer.seconds);
-            document.getElementById('timer').innerHTML = "Time Lapsed: " + timer.seconds + "s";
-            timer.changeName();
-            timer.seconds ++;
+  clickStart: function (){
+    this.start.addEventListener("click", function(){
+          if (timer.start.textContent === "Start"){
+            timer.timerID = setInterval(function(){
+              timer.seconds ++;
+              timer.timerElement.innerHTML = "Time Lapsed: " + timer.seconds + "s";
             },1000);
+            timer.changeName();
           }
-          else if (timer.seconds > 0 || timer.start.textContent === "Stop"){
-            var stop = document.querySelectorAll("button")[1].id;
-            clearInterval(timerID);
-            console.log("You logged" + timer.stop + "seconds");
-            document.getElementById('timer').innerHTML = "You logged: " + timer.seconds + "s";
+          else if (timer.start.textContent === "Stop"){
+            clearInterval(timer.timerID);
+            timer.timerElement.innerHTML = "You logged: " + timer.seconds + "s";
             timer.changeBack();
           }
-  });
-},
+    });
+  },
 
   resetClock: function() {
-      timer.reset.addEventListener("click", function() {
-        var stop = document.querySelectorAll("button")[1].id;
-        stop = 0;
-        clearInterval(timerID);
-        console.log("You logged" + timer.stop + "seconds");
+      this.reset.addEventListener("click", function() {
+        clearInterval(timer.timerID);
         timer.seconds = 0;
-        document.getElementById('timer').innerHTML = "Stop Watch";
+        timer.timerID = 0;
+        timer.timerElement.innerHTML = "Stop Watch";
         timer.changeBack();
     });
-},
+  },
 
   pauseClock: function() {
-    pause.addEventListener("click", function() {
-      var stop = document.querySelectorAll("button")[1].id;
-      console.log("You are stopped at" + timer.stop + "seconds");
-      clearInterval(timerID);
-      timer.seconds = stop;
-      document.getElementById('timer').innerHTML = "Time Lapsed: " + timer.seconds + "s";
+    this.pause.addEventListener("click", function() {
+      clearInterval(timer.timerID);
+      timer.timerElement.innerHTML = "Time Lapsed: " + timer.seconds + "s";
       timer.changeBack();
     });
-   }
+  }
 };
 
 timer.clickStart();
 timer.pauseClock();
 timer.resetClock();
-
-
-
-
 /*
-var timer = {
-  start: document.getElementById('start'),
+var start =  document.getElementById('start');
+var reset =  document.getElementById('reset');
+var stop =   document.getElementById('start');
+var pause =  document.getElementById('pause');
+var timerSeconds =  document.getElementById('timer');
+var seconds = 0;
+var minutes = 0;
+var timerID = 0;
 
-  pause: document.getElementById('pause'),
-
-  reset: document.getElementById('reset'),
-
-  startTimer: function (){
-    var seconds = 0;
-    var totaltime = [];
-    var timerCount = setInterval(function(){
-      start.textContent = "Stop";
-      document.getElementById('timer').textContent = "Time Lapsed: " + seconds + "s";
-
-      seconds ++;
-    }, 1000);
-  },
-  pauseTimer: function (){
-    clearInterval(clickStart);
-  }
-};
-var clickStart = start.addEventListener("click", timer.startTimer);
+function startTiming(clicking){
+    timeID = setInterval(clicking, 1000);
+}
+function clicking {
+  changeName ();
+  timerSeconds.innerHTML = "Time Lapsed: " + seconds + "s";
+  start.setAttribute("id", timerID);
+  seconds++;
+}
+function clearTiming(){
+  clearInterval(timeID);
+}
+function changeName(){
+  start.textContent = "Stop";
+}
+function changeBack(){
+  start.textContent = "Start";
+}
+function clickStart(){
+    start.addEventListener("click", function(){
+      if(seconds <= 0 || start.textContent === "Start"){
+      changeName ();
+      timerSeconds.innerHTML = "Time Lapsed: " + seconds + "s";
+      start.setAttribute("id", timerID);
+      seconds++;
+    }
+      else if(seconds > 1 || start.textContent === "Stop"){
+      clearTiming();
+      changeBack();
+      var stop = document.querySelectorAll("button")[1].id;
+      timerSeconds.innerHTML = "You logged: " + parseInt(stop) + "s";
+    }
+  });
+}
+startTiming(clickStart);
 */
