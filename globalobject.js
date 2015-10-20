273a9a4901427983d1aab1cf37f9dd01dddb95e1
Ly Nguyen <Ly900@aol.com>
2015-10-20 00:53:38 -0400

    put timer in global object

---
+++ globalobject.js
@@ -0,0 +1,34 @@
+var myStopWatch = function() {
+
+var resetButton = document.querySelector("#reset");
+var startButton = document.querySelector("#start");
+var pauseButton = document.querySelector("#pause");
+var h1 = document.querySelector("#timer");
+var h1new = h1.cloneNode(true);
+var seconds = 0;
+var timerId;
+
+function updateTime() {
+    h1.textContent = "Time Elapsed: " + seconds;
+    newSeconds = seconds++;
+    h1.textContent = "Time Elapsed: " + newSeconds;
+};
+
+function interval() {
+  secondsTimer = setInterval(updateTime, 1000);
+};
+
+startButton.addEventListener("click", interval);
+
+pauseButton.addEventListener("click", function(){
+  clearInterval(secondsTimer);
+});
+
+resetButton.addEventListener("click", function(){
+  clearInterval(secondsTimer);
+  seconds = 0;
+  h1.textContent = h1new.textContent;
+});
+
+} //ends myStopWatch object
+myStopWatch(); 
--- jstestfile.js
+++
@@ -1,4 +0,0 @@
-var resetButton = document.querySelector("#reset");
-var startButton = document.querySelector("#start");
-var pauseButton = document.querySelector("#pause");
-var h1 = document.querySelector("#timer");
