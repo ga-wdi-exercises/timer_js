4457b9ddb267c06a24ff2e144e75ed6d68c2e273
Starr Z. Chen <chenstarr@gmail.com>
2015-10-20 18:48:49 -0400

    Fix cumulative timer issue

--- timers.js
+++ timers.js
@@ -1,29 +1,35 @@
 var seconds = 0;
 var timerId = 0;
 var timerDisplay = document.getElementById("timer");
+var numClicks = 0;
 
 var reset = document.getElementById("reset");
 var resetClickEvent = function(){
   clearInterval(timerId);
   seconds = 0;
   document.getElementById("timer").innerHTML = "Stop Watch";
+  numClicks = 0;
 }
 reset.onclick = resetClickEvent;
 
 var start = document.getElementById("start");
 var startClickEvent = function(){
-  timerDisplay.innerHTML = seconds;
-  timerId = setInterval(updateTime, 1000)
+  numClicks++
+  if (numClicks == 1) {
+    timerDisplay.innerHTML = (seconds + " seconds");
+    timerId = setInterval(updateTime, 1000);
+  }
 }
 start.onclick = startClickEvent;
 
 var pause = document.getElementById("pause");
 var pauseClickEvent = function(){
   clearInterval(timerId);
+  numClicks = 0;
 }
 pause.onclick = pauseClickEvent;
 
 function updateTime() {
-  seconds++; //are the seconds in milliseconds?
-  timerDisplay.innerHTML = seconds;
+  seconds++;
+  timerDisplay.innerHTML = seconds + " seconds";
 }
