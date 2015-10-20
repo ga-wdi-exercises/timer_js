56147e30a264616bc999016f52bb4467540d124c
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-10-19 22:47:18 -0400

    timer with bonus

--- timers.js
+++ timers.js
@@ -1,34 +1,38 @@
-var reset = document.querySelector("#reset");
-var start = document.querySelector("#start");
-var pause = document.querySelector("#pause");
-
-start.addEventListener("click", startTimer);
-reset.addEventListener("click", resetTimer);
-pause.addEventListener("click", pauseTimer);
-
-var seconds = 0;
-var timerHeader = document.getElementById("timer");
-var intervalID;
-
-function updateTime() {
-	seconds++;
-	timerHeader.innerText = seconds;
+var timer = {
+  reset: document.querySelector("#reset"),
+  start: document.querySelector("#start"),
+  pause: document.querySelector("#pause"),
+  seconds: 0,
+  timerHeader: document.getElementById("timer"),
+  intervalID: true,
+  updateTime: function() {
+    timer.seconds++;
+    timer.timerHeader.innerText = timer.seconds;
+  },
+  startTimer: function() {
+    timer.start.addEventListener("click", function() {
+      timer.updateTime();
+      timer.timerHeader.innerText = timer.seconds;
+      timer.intervalID = setInterval(timer.updateTime, 1000);
+      return timer.intervalID;
+    })
+  },
+  pauseTimer: function() {
+    timer.pause.addEventListener("click", function() {
+      timer.seconds = timer.timerHeader.innerText;
+      clearInterval(timer.intervalID);
+      return timer.seconds;
+    })
+  },
+  resetTimer: function() {
+    reset.addEventListener("click", function() {
+      timer.seconds = 0;
+      timer.timerHeader.innerText = "Stop Watch";
+      clearInterval(timer.intervalID);
+    })
+  }
 }
-function startTimer() {
-	updateTime();
-	timerHeader.innerText = seconds;
-	intervalID = setInterval(updateTime, 1000);
-	return intervalID;
-}
 
-function pauseTimer() {
-	seconds = timerHeader.innerText;
-	clearInterval(intervalID);
-	return seconds;
-}
-
-function resetTimer() {
-	seconds = 0;
-	timerHeader.innerText = "Stop Watch";
-	clearInterval(intervalID);
-}
+timer.startTimer();
+timer.pauseTimer();
+timer.resetTimer();
