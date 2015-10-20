588ea042ee900edf20b7edb17d65e18dcf813dd9
jen pengelly <jgpengelly@gmail.com>
2015-10-20 19:03:09 -0400

    Added functions for Start click

--- timers.js
+++ timers.js
@@ -1,25 +1,28 @@
 // Step 1: Create JS selectors to target each of the timer buttons
-var resetButton = document.querySelector("#reset");
-var startButton = document.querySelector("#start");
-var pauseButton = document.querySelector("#pause");
+var reset = document.querySelector("#reset");
+var start = document.querySelector("#start");
+var pause = document.querySelector("#pause");
 var timer = document.querySelector("#timer");
-// Step 2. create click handlers for the timer buttons
-//resetButton.addEventListener("click",);
-startButton.addEventListener("click", updateTime);
-//startButton.addEventListener("click", );
-//pauseButton.addEventListener("click",);
 
 //  Step 3: Instantiate seconds and timerId veriables
 var seconds = 0;
 var timerId = 0;
 
+
+// Step 2. create click handlers for the timer buttons
+//reset.addEventListener("click",);
+
+//pause.addEventListener("click",);
+
+
+
 // Step 4: Create function updateTime
   // increment by seconds (1)
   // insert value of seconds into <h1> element with id="#timer"
 
-function updateTime() {
+
+function updateTime () {
   seconds++;
-  timer.innerHTML = "Time elapsed: " + seconds;
 }
 
 // Start Functionality
@@ -27,6 +30,13 @@ function updateTime() {
     //  replace text "Stop Watch" with "Time elapsed: 0"
     // increase count by 1 second.
 
+    function startClick () {
+      timer.innerHTML = "Time elapsed: " + seconds;
+      var secIncrement = seconds++;
+      timerId = setInterval(updateTime, 1000)
+    }
+
+    start.addEventListener("click", startClick);
 
 // Reset Functionality
   // click reset
