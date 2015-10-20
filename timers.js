5b417e702ffa82796481b153bd08e10bbbfc541e
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2015-10-20 18:04:53 -0400

    Timer Complete

--- timers.js
+++ timers.js
@@ -1,4 +1,32 @@
+var timerStart = document.querySelector('#start');
+var timer = document.querySelector('#timer');
+var timerPause = document.querySelector('#pause');
+var timerReset = document.querySelector('#reset');
 
+var seconds = 0;
+var songTimer;
 
+var updateTimer = function() {
+  seconds++;
+  timer.textContent = seconds
+}
 
+var startTimer = function () {
+  timer,textContent = seconds
+  songTimer = setInterval(updateTimer, 1000)
+  timerStart.removeEventListener("click", startTimer)
+}
 
+timerStart.addEventListener('click', startTimer)
+
+timerPause.addEventListener("click", function(){
+ clearInterval(songTimer)
+ start.addEventListener("click", startTimer)
+});
+
+timerReset.addEventListener("click", function(){
+ clearInterval(songTimer)
+ seconds = 0
+ timer.textContent = 'Stop Watch'
+ start.addEventListener("click", startTimer)
+});
