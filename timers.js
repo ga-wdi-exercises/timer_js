9fe4a7a711696218b7cd8049e1e8217cb6b0807f
Peter Zhang <pzhang87@gmail.com>
2015-10-20 08:37:29 -0400

    complete exercise

--- timers.js
+++ timers.js
@@ -1,4 +1,47 @@
+//create selectors that target the buttons
+var resetButton = document.querySelector("#reset");
+var startButton = document.querySelector("#start");
+var pauseButton = document.querySelector("#pause");
+
+displayTime = document.querySelector("h1")
+displayTime.id = "#timer"
+
+var seconds = 0;
+var timerId;
+var isRunning = false;
+
+//create click handlers
+
+var resetTimer = function() {
+  console.log("reset");
+  clearInterval(timerId);
+  seconds = 0;
+  displayTime.textContent = "Stop Watch"
+  isRunning = false;
+};
+
+var startTimer = function() {
+  while (!isRunning) {
+  console.log("start");
+  isRunning = true;
+  timerId = setInterval(updateTime, 1000);
+    }
+  }
+
+var pauseTimer = function() {
+  console.log("pause");
+  clearInterval(timerId);
+  isRunning = false;
+}
+
+resetButton.addEventListener("click", resetTimer);
+startButton.addEventListener("click", startTimer);
+pauseButton.addEventListener("click", pauseTimer);
 
 
+// 4. Create an `updateTime()` function that increments the `seconds` counter and inserts that value into the `<h1>` element with `id="timer"`.
 
-
+var updateTime = function() {
+  displayTime.textContent = seconds;
+  seconds++;
+}
