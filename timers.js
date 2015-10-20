e00928f3401da8156c897c9820f8726f4e648ee6
Ly Nguyen <Ly900@aol.com>
2015-10-19 20:28:23 -0400

    finish 1, 2, and 3

--- timers.js
+++ timers.js
@@ -1,4 +1,13 @@
+// 1.
+var resetButton = document.querySelector("#reset");
+var startButton = document.querySelector("#start");
+var pauseButton = document.querySelector("#pause");
 
+// 2.
+resetButton.addEventHandler();
+startButton.addEventHandler();
+pauseButton.addEventHandler();
 
-
-
+// 3.
+var seconds;
+var timerID;
