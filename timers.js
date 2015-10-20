9d882c455f63a3ce1026fab6e58ac7eb32a54e48
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-20 08:58:30 -0400

    got 50% done

--- timers.js
+++ timers.js
@@ -1,4 +1,28 @@
 
+//1
+ var start = document.querySelector("#start");
+  var stop = document.querySelector("#pause");
+   var reset = document.querySelector("#reset");
 
+  //3
+var seconds;
+var timerId;
 
+var clickStart = function(){
+document.body.className ="controls"
+}
+//2
+start.addEventListener("click", function(){
 
+});
+
+//4
+document.getElementsById("timer").innerHTML = seconds;
+function updateTime() {
+  console.log("update ran")
+}
+// setInterval(updateTime, 100)
+
+stop.addEventListener("click", function(){
+  clearInterval(timerId)
+});
