db628c445bb7b57e45b8c2285a70b94cc5e2b6b1
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-10-20 12:37:10 -0400

    Initial Add - Mia's Timer

--- timers.js
+++ timers.js
@@ -1,4 +1,56 @@
+// //create javascript selectors that target each of the timer buttons
+// var resetButton  = document.querySelector("button");
+// var handleClickEvent = function (){
+//     console.log("I was clicked!")
+// }
+//
+// button.addEventListener("click", handleClickEvent())
+//
+// // var startButton = document.getElementById("start");
+// //
+// // var pauseButton = document.getElementById("pause");
+// //
+// // //create click handlers for each of the timer buttons
+
+
+//#3 something later to initiate - global variables
+
+var seconds = 0
+var timerId = 0
+
+var updateTime = function (){
+seconds = seconds + 1
+document.getElementById('timer').innerHTML = seconds;
+}
+
+
+var button = document.getElementById("reset");
+var handleClickEvent = function(){
+  console.log("I have been clicked")
+}
+button.addEventListener("click", handleClickEvent)
+
+//#5 - replace stop watch in the html with the content of teh seconds variable. Use setInterval to increment the timer by 1 every second
+var button = document.getElementById("start");
+var handleClickEvent = function(){
+  console.log("I have also been clicked")
+var setInterval = function (){
+//use timerID
+  }
+}
+button.addEventListener("click", handleClickEvent)
 
 
 
 
+var button = document.getElementById("pause");
+var handleClickEvent = function(){
+  console.log("Don't forget about me being clicked!!")
+}
+button.addEventListener("click", handleClickEvent)
+
+
+
+//take seconds var from up top, u
+// insert this value into teh h1 element with id = timer
+};
