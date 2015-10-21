9366aeaccb964fc2ca6be70c94c360dd1af509a5
Thom Householder <thomhouseholder@gmail.com>
2015-10-21 08:52:07 -0400

    project thus far before study session

--- timers.js
+++ timers.js
@@ -1,4 +1,51 @@
 
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var pause = document.getElementById('pause');
+// in progress
+var seconds=setInterval(startClickEvent, 3000)
+var timerId;
+
+var startClickEvent = function(){
+  console.log("Start was clicked!")
+  updateTime()
+}
+var resetClickEvent = function(){
+  console.log("Reset was clicked!")
+}
+var pauseClickEvent = function(){
+  console.log("Pause was clicked!")
+}
+
+  start.addEventListener("click", startClickEvent);
+  reset.addEventListener("click", resetClickEvent);
+  pause.addEventListener("click", pauseClickEvent);
+
+function updateTime() {
+  document.getElementById('timer').innerHTML='what is this?'
+}
+//
+//   function startCounting(){
+//      console.log(seconds)
+//     }
+//
+//     setInterval(startCounting, 1000);
+//
+//
+// function change() {
+//    document.querySelector('h1').innerHTML('seconds')
+// }
+//
 
 
 
+
+//
+// var button = document.querySelector("button")
+// var handleClickEvent = function(){
+//   console.log("I was clicked!")
+// }
+//
+// button.onclick = handleClickEvent
+// //or
+// button.addEventListener("click", handleClickEvent)
