7d2d77b75f8f30e124b9693ad1d4fa02a52c22a4
Teddy Kentor <tkentor@gmail.com>
2015-10-21 07:48:47 -0400

    timer js hw

--- timers.js
+++ timers.js
@@ -1,4 +1,27 @@
+var counter = 0
+var updateTime = function () {
+  counter++;
+  console.log("Time Elapsed: " + counter);
+}
 
+var start = document.getElementById('start');
+start.addEventListener("click", function (){
+    window.setInterval(updateTime, 1000)
+    // start.removeEventListener("click", function (){
+    // window.clearInterval(updateTime, 0)
+    // });
+});
 
+var pause = document.getElementById('pause');
+pause.addEventListener("click", function (){
+    console.log("stopped")
+    // document.getElementById('start');
+    // start.removeEventListener("click", function (){
+    // window.clearInterval(updateTime, false)
+//    })
+})
 
-
+var reset = document.getElementById('reset');
+reset.addEventListener("click", function (){
+    console.log("reset")
+})
