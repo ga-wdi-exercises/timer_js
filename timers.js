1fb215710b0fe863666f2a0a0254a7388e69ed87
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-21 08:49:21 -0400

    Initial commit

--- timers.js
+++ timers.js
@@ -1,4 +1,76 @@
+/******************************************
+BONUS VERSION (unfinished)
+******************************************/
+//
+// var timer = {
+//   createTimer: [
+//     stopWatch = document.getElementById("timer"),
+//     reset = document.getElementById("reset"),
+//     start = document.getElementById("start"),
+//     pause = document.getElementById("pause"),
+//     countSec = 0
+//   ],
+//   seconds: function () {
+//     if (countSec < 2) {
+//       stopWatch.innerHTML = "Time Elapsed: " + countSec + " second.";
+//       countSec += 1;
+//     } else {
+//       stopWatch.innerHTML = "Time Elapsed: " + countSec + " seconds.";
+//       countSec += 1;
+//     }
+//   },
+//   start: function () {
+//     start.addEventListener("click", function() {
+//       countStart = setInterval(seconds, 1000);
+//     } );
+//   },
+//   pause: function () {
+//     pause.addEventListener("click", function() {
+//       clearInterval(countStart);
+//     } );
+//   },
+//   reset: function () {
+//     reset.addEventListener("click", function() {
+//       clearInterval(countStart);
+//       countSec = 0;
+//       stopWatch.innerHTML = "Stop Watch";
+//     } );
+//   }
+// }
+
+/******************************************
+ORIGINAL
+******************************************/
 
 
+var stopWatch = document.getElementById("timer");
+var reset = document.getElementById("reset");
+var start = document.getElementById("start");
+var pause = document.getElementById("pause");
 
+var timerId;
+var seconds = 0;
 
+var updateTime = function () {
+  seconds += 1;
+  if (seconds < 2) {
+    stopWatch.innerHTML = "Time Elapsed: " + seconds + " second.";
+  }
+  else {
+    stopWatch.innerHTML = "Time Elapsed: " + seconds + " seconds.";
+  }
+}
+
+start.addEventListener("click", function() {
+  countStart = setInterval(updateTime, 1000);
+} );
+
+pause.addEventListener("click", function() {
+  clearInterval(countStart);
+} );
+
+reset.addEventListener("click", function() {
+  clearInterval(countStart);
+  seconds = 0;
+  stopWatch.innerHTML = "Stop Watch";
+} );
