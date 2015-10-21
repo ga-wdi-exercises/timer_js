504cefec77069e1692b5b31928395dd119a9c1ce
Tory Burgett <vdburgett@gmail.com>
2015-10-21 09:16:40 -0400

    Second Commit

--- timers.js
+++ timers.js
@@ -1,24 +1,40 @@
 //Variables seconds & timerID
 var seconds = 0;
 var timerID;
+var stopWatch;
+
+function updateTime(){
+  seconds++;
+  var timerIDSelector = document.querySelector("#timer");
+  timerIDSelector.textContent = "Stop Watch " +  seconds;
+}
+function stopTime(){
+  clearInterval(stopWatch);
+}
+
+
 
 //Reset Button
 var resetButton = document.querySelector("#reset");
 var handleClickEventReset = function(){
   console.log("I was clicked! RESET");
+  seconds = 0;
+  var timerIDSelector = document.querySelector("#timer");
+  timerIDSelector.textContent = "Stop Watch " +  seconds;
 };
 
 //Start Button
 var startButton = document.querySelector("#start");
 var handleClickEventStart = function(){
   console.log("I was clicked! START");
-  setInterval();
+  stopWatch = setInterval(updateTime, 1000);
 };
 
 //Stop Button
 var stopButton = document.querySelector("#pause");
 var handleClickEventStop = function(){
   console.log("I was clicked! STOP");
+  stopTime();
   var timerIDSelector = document.querySelector("#timer");
   timerIDSelector.textContent = "Stop Watch " +  seconds;
 };
@@ -28,12 +44,8 @@ resetButton.addEventListener("click", handleClickEventReset);
 startButton.addEventListener("click", handleClickEventStart);
 stopButton.addEventListener("click", handleClickEventStop);
 
-var updateTime = function(){
-  seconds++;
-  var timerIDSelector = document.querySelector("#timer");
-  timerIDSelector.textContent = "Stop Watch " +  seconds;
-};
-setInterval(function(){updateTime(); }, 1000);
+
+
 
 // //ADAM CODE
 // function startTimerApp() {
