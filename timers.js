69a15bd9c0883c488d45a3bc55f1a5fb02273be3
Timothy Gregg <timtgregg@gmail.com>
2015-10-20 21:42:10 -0400

    init

--- timers.js
+++ timers.js
@@ -1,4 +1,11 @@
+var timer = {
 
+};
 
+console.log(performance.now());
 
+console.log(Date.now());
 
+var reset = document.getElementById('reset');
+var start = document.getElementById('start');
+var pause = document.getElementById('pause');
