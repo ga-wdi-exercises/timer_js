d9949496fdf70714de1bb7700e701d1962016843
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-10-20 21:42:21 -0400

    timers working code

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+  var resetButton = document.querySelector("#reset");
+  var startButton = document.querySelector("#start");
+  var pauseButton = document.querySelector("#pause");
+  var stopWatch = document.querySelector("#timer");
+  var clicks = 0;
+  var seconds = 0;
+
+  function updateTime() {
+    stopWatch.innerHTML = ("Time Elapsed: " + seconds++);
+  };
 
 
+  startButton.addEventListener("click", function(){
+    clicks++;
+    if (clicks < 2) {
+      timerId = setInterval(updateTime, 1000);
+    }
+  });
 
+  pauseButton.addEventListener("click", function (){
+    clearInterval(timerId);
+  });
 
+  resetButton.addEventListener("click", function (){
+    clearInterval(timerId);
+    stopWatch.innerHTML = "Stop Watch";
+    seconds = 0;
+    clicks= 0;
+  });
