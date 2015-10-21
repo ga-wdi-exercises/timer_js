4ba886443e7284bb25c4ea7707ff8330c335d589
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-21 13:44:58 -0400

    Completed bonus assignment

--- timers.js
+++ timers.js
@@ -1,76 +1,84 @@
 /******************************************
-BONUS VERSION (unfinished)
+BONUS VERSION WITH JQUERY
 ******************************************/
-//
-// var timer = {
-//   createTimer: [
-//     stopWatch = document.getElementById("timer"),
-//     reset = document.getElementById("reset"),
-//     start = document.getElementById("start"),
-//     pause = document.getElementById("pause"),
-//     countSec = 0
-//   ],
-//   seconds: function () {
-//     if (countSec < 2) {
-//       stopWatch.innerHTML = "Time Elapsed: " + countSec + " second.";
-//       countSec += 1;
-//     } else {
-//       stopWatch.innerHTML = "Time Elapsed: " + countSec + " seconds.";
-//       countSec += 1;
-//     }
-//   },
-//   start: function () {
-//     start.addEventListener("click", function() {
-//       countStart = setInterval(seconds, 1000);
-//     } );
-//   },
-//   pause: function () {
-//     pause.addEventListener("click", function() {
-//       clearInterval(countStart);
-//     } );
-//   },
-//   reset: function () {
-//     reset.addEventListener("click", function() {
-//       clearInterval(countStart);
-//       countSec = 0;
-//       stopWatch.innerHTML = "Stop Watch";
-//     } );
-//   }
-// }
+
+var timer = {
+  init: function() {
+    stopWatch = $("#timer");
+    reset = $("#reset");
+    start = $("#start");
+    pause = $("#pause");
+    seconds = 0;
+    start.on("click", function () {
+      timer.startTimer();
+    });
+    pause.on("click", function () {
+      timer.pauseTimer();
+    });
+    reset.on("click", function () {
+      timer.resetTimer();
+    });
+  },
+  updateTime: function() {
+    seconds += 1;
+    if (seconds < 2) {
+      stopWatch.html("Time Elapsed: " + seconds + " second.");
+    }
+    else {
+      stopWatch.html("Time Elapsed: " + seconds + " seconds.");
+    }
+  },
+  startTimer: function() {
+    countStart = setInterval(this.updateTime, 1000);
+  },
+  pauseTimer: function() {
+    clearInterval(countStart);
+  },
+  resetTimer: function() {
+    clearInterval(countStart);
+    seconds = 0;
+    stopWatch.html("Stop Watch");
+  }
+}
+
+timer.init()
+
 
 /******************************************
 ORIGINAL
 ******************************************/
 
 
-var stopWatch = document.getElementById("timer");
-var reset = document.getElementById("reset");
-var start = document.getElementById("start");
-var pause = document.getElementById("pause");
-
-var timerId;
-var seconds = 0;
-
-var updateTime = function () {
-  seconds += 1;
-  if (seconds < 2) {
-    stopWatch.innerHTML = "Time Elapsed: " + seconds + " second.";
-  }
-  else {
-    stopWatch.innerHTML = "Time Elapsed: " + seconds + " seconds.";
-  }
-}
-
-start.addEventListener("click", function() {
-  countStart = setInterval(updateTime, 1000);
-} );
-
-pause.addEventListener("click", function() {
-  clearInterval(countStart);
-} );
-
-reset.addEventListener("click", function() {
-  clearInterval(countStart);
-  seconds = 0;
-  stopWatch.innerHTML = "Stop Watch";
-} );
+// var stopWatch = document.getElementById("timer");
+// var reset = document.getElementById("reset");
+// var start = document.getElementById("start");
+// var pause = document.getElementById("pause");
+//
+// // var timerId;
+// var seconds = 0;
+//
+// var updateTime = function () {
+//   seconds += 1;
+//   if (seconds < 2) {
+//     stopWatch.innerHTML = "Time Elapsed: " + seconds + " second.";
+//   }
+//   else {
+//     stopWatch.innerHTML = "Time Elapsed: " + seconds + " seconds.";
+//   }
+// }
+//
+// start.addEventListener("click", function() {
+//   // if (countStart !== setInterval(updateTime, 1000)) {
+//     countStart = setInterval(updateTime, 1000);
+//   // }
+// } );
+//
+// pause.addEventListener("click", function() {
+//   clearInterval(countStart);
+// } );
+//
+// reset.addEventListener("click", function() {
+//   clearInterval(countStart);
+//   seconds = 0;
+//   stopWatch.innerHTML = "Stop Watch";
+// } );
