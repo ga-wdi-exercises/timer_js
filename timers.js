7eb48c1605883defb2244a0530cf0337de13deb9
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-10-21 09:00:57 -0400

    my homework

--- timers.js
+++ timers.js
@@ -1,4 +1,26 @@
+// alert("This Works!");
+var startButton = document.querySelector("#start");
+var resetButton = document.querySelector("#reset");
+var pauseButton = document.querySelector("#pause");
 
+var startClickEvent = function(){
+interval = setInterval(updateTime, 1000);
+ };
+var updateTime = function(){
+  seconds++;
+  timer.textContent = "Time Elapsed: " + seconds;
+};
+var seconds = 0;
 
+startButton.addEventListener("click", startClickEvent);
 
+// var pauseButton = document.querySelector("pause");
+var timer = document.querySelector("#timer");
+var interval;
+var resetClickEvent = function(){
+  clearInterval(interval);
+};
+resetButton.addEventListener("click", resetClickEvent);
 
+// var seconds =
+// var timerId =
