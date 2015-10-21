19686c57dd0ac9cb5d26d52754ff7c0a802fd5d7
Matiullah Amin <matiullah243@gmail.com>
2015-10-20 20:57:40 -0400

    finished the timer HW

--- timers.js
+++ timers.js
@@ -1,4 +1,35 @@
+var pauseButton = document.getElementById("pause");
+var resetButton = document.getElementById("reset");
+var startButton = document.getElementById("start");
+var seconds = 0;
+var timerId = 0;
+/* START Button*/
+startButton.addEventListener ("click", function (){
+  console.log ("I've starting the timer");
+  if (timerId === 0) {
+    document.getElementById("timer").textContent="Time elapsed: ";
+    timerId = setInterval(updateTime, 1000);
+  }
+  });
 
+  /* RESET Button*/
+  resetButton.addEventListener ("click", function (){
+    console.log ("I've reset your timer to 0");
+    document.getElementById("timer").textContent="Stop Watch";
+    clearInterval(timerId);
+    seconds = 0;
+    //document.getElementById("timer").textContent="Stop Watch";
+  });
 
+  /* PAUSE Button*/
+  pauseButton.addEventListener ("click", function (){
+    console.log ("I've paused it for you. You can restart or reset to 0");
+    clearInterval(timerId);
+    timerId = 0;
+  });
 
-
+  /*UpdateTime Function*/
+  var updateTime = function() {
+    seconds++;
+    document.getElementById("timer").textContent = "Time elapsed: " + seconds;
+  };
