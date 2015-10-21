fd7e376a45c683ef9375b9ca10d4a83ff47f3efb
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-21 16:58:40 -0400

    added another seconds and minute column to make it look more like a stopwatch

--- timers.js
+++ timers.js
@@ -6,19 +6,32 @@ resetButton.addEventListener("click",resetButtonClick)
 startButton.addEventListener("click",startButtonClick)
 pauseButton.addEventListener("click",pauseButtonClick)
 
-var seconds = 0
+var seconds0 = 0
+var seconds1 = 0
+var minutes =0
 var timerId
 var h1Tag = document.getElementById('timer');
 
 function updateTime(){
-  seconds = seconds+1
-  h1Tag.textContent = seconds
+  seconds0 = seconds0+1
+  if(seconds0>9){
+    seconds1 += 1
+    seconds0 =00
+    if(seconds1>5){
+      minutes+=1
+      seconds0=0
+      seconds1=0
+    }
+  }
+  h1Tag.textContent = +minutes+ ":" + seconds1+seconds0
 }
 
 function resetButtonClick(){
   startButton.addEventListener("click",startButtonClick)
   timerId = window.clearInterval(timerId)
-  seconds = 0;
+  seconds0 = 0;
+  seconds1 = 0;
+  minutes =0;
   h1Tag.textContent = "Stop Watch"
 }
 
