7a62a6c2da801e5f7b69c1a3f1e4dab58240fa93
Thom Householder <thomhouseholder@gmail.com>
2015-10-22 18:02:26 -0400

    start button runs timer and displays on screen

--- timers.js
+++ timers.js
@@ -6,62 +6,24 @@ var pause = document.getElementById('pause');
 
 //selectors for each timer button
 start.addEventListener("click", startClickEvent);
-reset.addEventListener("click", resetClickEvent);
-pause.addEventListener("click", pauseClickEvent);
+// reset.addEventListener("click", resetClickEvent);
+// pause.addEventListener("click", pauseClickEvent);
 
-//event handlers
-var startClickEvent = function(){
-  console.log("Start was clicked!")
-  //timer
-  var seconds=setInterval(click, 1000)
-  document.getElementById('timer').innerHTML=seconds
-  //make sure timer restarts when Start is clicked again
-}
-var resetClickEvent = function(){
-  console.log("Reset was clicked!")
-  //stop timer with clearInterval()
-  //reset timer to zero
-  //replace time with original Stop Watch text
-  document.getElementById('timer').innerHTML='Stop Watch'
-}
-var pauseClickEvent = function(){
-  console.log("Pause was clicked!")
-  //create clearInterval() to reset timer - stop but don't reset
-  clearInterval(seconds)
+//start button first click - timer start
+var seconds=0;
+function startClickEvent(evt) {
+  //set clock count
+
+  var timerId=setInterval(updateTime, 1000)
 }
 
 
-// update seconds variable
-function updatTime() {
-
+function updateTime(){
+//increment counter and update screen
+  seconds++
+  document.querySelector('#timer').innerHTML=seconds
 }
 
-//pass seconds update to
-function updateTime() {
-  document.getElementById('timer').innerHTML=seconds
-}
-//
-//   function startCounting(){
-//      console.log(seconds)
-//     }
-//
-//     setInterval(startCounting, 1000);
-//
-//
-// function change() {
-//    document.querySelector('h1').innerHTML('seconds')
-// }
-//
 
 
-
-
-//
-// var button = document.querySelector("button")
-// var handleClickEvent = function(){
-//   console.log("I was clicked!")
-// }
-//
-// button.onclick = handleClickEvent
-// //or
-// button.addEventListener("click", handleClickEvent)
+console.log('seconds', seconds++);
