d9bdafd38add0d3935f46048aa7fb06bdb09ac61
Thomas Clark <tclark@soteriagiving.com>
2015-12-30 11:13:00 -0500

    fixed typos causing bad functionality

--- timers.js
+++ timers.js
@@ -9,24 +9,24 @@ var timer =  {
     pause: document.getElementById("pause"),
   },
 
-updateTime: function() {
+updateTime: function updateTimer() {
   this.seconds++;
   this.els.timer.textContent = 'Time elapsed: ' + this.seconds;
 },
 
 setupListeners: function () {
   this.els.start.addEventListener('click', function() {
-    alert('start');
+
     this.els.timer.textContent = 'Time Elapsed: ' + this.seconds;
     this.timerId = setInterval(this.updateTime.bind(this), 1000);
   }.bind(this));
 
-  this.els.pause.addEventListner('click', function () {
-    clarInterval(this.timerId);
+  this.els.pause.addEventListener('click', function () {
+    clearInterval(this.timerId);
   }.bind(this));
 
   this.els.reset.addEventListener('click', function() {
-    alert('reset');
+
     this.seconds = 0;
     clearInterval(this.timerId);
     this.els.timer.textContent = 'Stop Watch';
