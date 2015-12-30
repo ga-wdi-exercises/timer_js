39b6e6408a9365ac513e8d447660b26d6d48b537
Thomas Clark <tclark@soteriagiving.com>
2015-12-30 10:51:13 -0500

    add dom selectors

--- timers.js
+++ timers.js
@@ -2,4 +2,17 @@ var timer =  {
   seconds: 0,
   timerId: 0,
 
+  els: {
+    timer: document.getElementById("timer"),
+    reset: document.getElementById("reset"),
+    start: document.getElementById("start"),
+    pause: document.getElementById("pause"),
+  },
+
+  updateTime: function updateTime(){
+    this.seconds++;
+    this.els.timer.textContent = 'Time elapsed: ' + this.seconds;
+  },
+  
+
 };
