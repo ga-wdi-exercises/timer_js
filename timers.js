984bdab0aa51738a34022fb730f429b4b37bbcb4
Thomas Clark <tclark@soteriagiving.com>
2015-12-30 11:14:16 -0500

    removed console.logs after debug

--- timers.js
+++ timers.js
@@ -20,13 +20,11 @@ setupListeners: function () {
     this.els.timer.textContent = 'Time Elapsed: ' + this.seconds;
     this.timerId = setInterval(this.updateTime.bind(this), 1000);
   }.bind(this));
-
   this.els.pause.addEventListener('click', function () {
     clearInterval(this.timerId);
   }.bind(this));
 
   this.els.reset.addEventListener('click', function() {
-
     this.seconds = 0;
     clearInterval(this.timerId);
     this.els.timer.textContent = 'Stop Watch';
