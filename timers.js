2c27435be801ddf10c1d18c488dc7656127c9822
Sean Tay <seantay@Seans-MacBook-Pro.local>
2016-05-31 18:34:21 -0400

    Initial commit

--- timers.js
+++ timers.js
@@ -1,4 +1,28 @@
+var start = $("#start");
+var reset = $("#reset");
+var pause = $("#pause");
+
+function countUp(){
+  ++seconds;
+$("h1").text("Time Lapse: " + seconds);
+}
+
+var seconds = 0;
+function startTimer() {
+  $("h1").text("Time Lapse:" + seconds);
+
+  var timer = setInterval(countUp,1000);
+
+  pause.on("click", function(){
+    clearInterval(timer);
+  })
+  reset.on("click", function(){
+    clearInterval(timer);
+    seconds = 0;
+    $("h1").text("Stop Watch");
+  })
+};
 
 
 
-
+start.on("click", startTimer);
