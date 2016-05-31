de1bb5df201d9f941aa164ecd0ed8a8764720686
Matthew Scott <matthewruelscott@gmail.com>
2016-05-31 19:34:44 -0400

    Made a timer

--- timers.js
+++ timers.js
@@ -1,4 +1,24 @@
+var reset = $("#reset");
+var start = $("#start");
+var pause = $("#pause");
+var seconds = 0;
+var timerId
 
+start.on("click",function(){
+ $("#timer").html("Time elapsed: "+seconds)
+   timerId = setInterval(function(){
+   seconds++;
+   $("#timer").html("Time elapsed: "+seconds)
+ },
+   1000);
+ });
 
+ pause.on("click", function(){
+   clearInterval(timerId)
+ })
 
-
+ reset.on("click", function(){
+   $("#timer").html("Stop Watch")
+   seconds = 0
+   clearInterval(timerId)
+ })
