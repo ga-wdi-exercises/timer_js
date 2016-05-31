8221cbfd855c769329964e6a01f3ac1319837e04
Anh Truong <anh.truong.07@cnu.edu>
2016-05-31 19:11:55 -0400

    IT WORKS! SHORT CLEAN CODE

--- timers.js
+++ timers.js
@@ -1,4 +1,27 @@
+var s = 0;
+var t;
+
+$("#start").on("click", startWatch); // Start
+
+function startWatch(){
+
+     t = setInterval(function(){
+      $("#timer").text(s); s++;
+    }, 1000);
+}
+
+$("#pause").on("click", pauseWatch); // pause
+
+function pauseWatch(){
+  window.clearInterval(t);
+  }
+
+$("#reset").on("click", resetWatch);
+//change text on reset .DONT START IT AGAIN OR IT WILL COUNT 2X
 
 
-
-
+function resetWatch(){
+  $("#timer").text("0");
+  s = 0;
+  startWatch();
+}
