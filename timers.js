41909290881689f0036f694a13aff5a75ae33187
Julia Schultz <julia.schultz91@gmail.com>
2016-05-31 19:21:44 -0400

    finishing stopwatch

--- timers.js
+++ timers.js
@@ -1,4 +1,40 @@
+var reset = $("#reset");
+var start = $("#start");
+var pause = $("#pause");
+var timerId;
+
+reset.on("click", resetTimer);
+start.on("click", startTimer);
+pause.on("click", pauseTimer);
 
 
+function startTimer() {
+  timerId = setInterval(updateTime, 1000);
+}
+
+var seconds = 0
+var minutes = 0
+
+function updateTime() {
+
+  seconds ++;
+
+  if (seconds >=60) {
+    seconds=0;
+    minutes ++
+  }
+
+  $("h1").text(minutes+" : "+seconds);
+
+}
+
+function pauseTimer() {
+    clearInterval(timerId);
+}
 
 
+  function resetTimer(){
+    clearInterval(timerId);
+    seconds=0;
+    $("h1").text("Stopwatch");
+  }
