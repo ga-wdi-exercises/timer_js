ea6dbb3e66d1c4eb7f8bf63630e9929d0d9eb86a
Mara Sanders <sandersmara@gmail.com>
2016-05-31 19:35:37 -0400

    Initial Commit Timer

--- timers.js
+++ timers.js
@@ -1,4 +1,27 @@
+// * When "Start" is clicked, the text "Stop Watch" should be replaced by "Time elapsed: 0", and the count should increment every second.
+// * When "Reset" is clicked, the text should return to "Stop Watch"
+// * When "Pause" is clicked, the text should say "Time elapsed: 1", but stop incrementing.
+var reset = $("#reset");
+var start = $("#start");
+var pause = $("#pause");
+var seconds = 0;
+var timerId
 
+start.on("click",function(){
+   $("#timer").html("Time elapsed: "+seconds)
+    timerId = setInterval(function(){
+    seconds++;
+    $("#timer").html("Time elapsed: "+seconds)
+  },
+    1000);
+  });
 
+  pause.on("click",function() {
+    clearInterval(timerId)
+  })
 
-
+  reset.on("click",function(){
+    $("#timer").html("Stop Watch")
+    seconds = 0
+    clearInterval(timerId)
+  })
