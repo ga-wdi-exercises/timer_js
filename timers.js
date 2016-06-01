2d03c796278e94472dee8577987d4cdfaa0571b3
Joe Calvillo <jcalvillo757@gmail.com>
2016-06-01 17:30:57 -0400

    Finished timer

--- timers.js
+++ timers.js
@@ -1,4 +1,62 @@
+var reset = $("#reset");
+var start = $("#start");
+var pause = $("#pause");
+var seconds = 0;
+var timerId;
+var timerIdReset = true;
+
+$(document).on("ready", function(){
+  start.on("click",function(){
+    if (timerIdReset == true){
+    timerId = setInterval(function(){
+      seconds++;
+    $("#timer").html("Time elapsed: " + seconds)
+      }, 100);
+      timerIdReset = false;
+   }
+});
+   pause.on("click", function(){
+     clearInterval(timerId)
+     timerIdReset = true;
+   })
+
+   reset.on("click", function(){
+     $("#timer").html("Stop Watch")
+     seconds = 0;
+     clearInterval(timerId)
+     timerIdReset = true;
+     $("#timer").html("Time elapsed: " + seconds)
+    })
+  });
 
 
+//Shorter code, same result.
+/*
+var seconds = 0;
+var timerId;
+var timerIdReset = true;
 
+$(document).on("ready", function(){
+  $("#start").on("click", function(){
+      if(timerIdReset == true){
+          timerId = setInterval(function(){
+              seconds += 1;
+              $("h1").text("Time elapsed: " + seconds);
+          }, 100);
+          timerIdReset = false;
+      }
+  });
 
+  $("#pause").on("click", function(){
+      clearInterval(timerId);
+      timerIdReset = true;
+  });
+
+  $("#reset").on("click", function(){
+      clearInterval(timerId);
+      timerIdReset = true;
+      seconds = 0;
+      $("h1").text("Time elapsed: " + seconds);
+  });
+});
+*/
