56dbdd5326f490e6c432dfc6f5fa3df233850c69
David <dcordz@gmail.com>
2016-06-01 14:06:43 -0400

    the start button makes it go up

--- timers.js
+++ timers.js
@@ -1,4 +1,9 @@
-
-
-
-
+$(document).ready(function() {
+  var time = 0;
+  $("#start").on("click", function() {
+    setInterval(function() {
+      time += 1;
+      $("#timer").html(time);
+    }, 1000);
+  });
+});
