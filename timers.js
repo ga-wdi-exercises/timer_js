e91e6e312d4182a05d537190bdd16d7721dcf1c1
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-01 14:10:46 -0400

    The Timer sort of works Reset, Start , Pause buttons have functionality

--- timers.js
+++ timers.js
@@ -1,4 +1,39 @@
+$("#start").on("click",function updateTime(){
+  var time = 0;
+  var count = setInterval(function(){
+    time= time + 1;
+    console.log(time);
+    $("#timer").text(time);
+  },1000)
+  $("#pause").on("click",function pauseClock(){
+    clearInterval(count);
+})
+  $("#reset").on("click",function resetClock(){
+    clearInterval(count);
+    $("#timer").text(0);
+  })
+})
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function resetClock(){
+//   console.log("Clock Reset");
+// }
+// function pauseClock() {
+//   console.log("Clock Paused");
+// }
