32e324a850482f7a343f852bedc2bc1529330eb6
Anh Truong <anh.truong.07@cnu.edu>
2016-06-01 10:54:34 -0400

    works

--- timers.js
+++ timers.js
@@ -1,19 +1,24 @@
 var s = 0;
 var t;
+var tm = true;
+
 
 $("#start").on("click", startWatch); // Start
 
 function startWatch(){
-
+    if (tm == true){ // added this if statement to prevent multiple intervals but wont start again after Pause
      t = setInterval(function(){
       $("#timer").text(s); s++;
     }, 1000);
+    tm = false;
+}
 }
 
 $("#pause").on("click", pauseWatch); // pause
 
 function pauseWatch(){
   window.clearInterval(t);
+  tm = true;
   }
 
 $("#reset").on("click", resetWatch);
@@ -21,7 +26,9 @@ $("#reset").on("click", resetWatch);
 
 
 function resetWatch(){
+  clearInterval(t); // added this line to make the completely stop
   $("#timer").text("0");
+  tm = true;
   s = 0;
-  startWatch();
+  // startWatch(); //took out this line to stop the button recounting
 }
