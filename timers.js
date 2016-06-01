50186a667572bc80b3c943ce7e8e3bb2bc6bfca0
Kevin Golden <kwg@cs.net>
2016-06-01 09:41:03 -0400

    It works

--- timers.js
+++ timers.js
@@ -1,4 +1,27 @@
 
+$(document).ready(function () {
 
+  seconds = 0;
+  timerID = "";
 
+  $("#start").on("click",doStart);
+  $("#pause").on("click",doPause);
+  $("#reset").on("click",doReset);
 
+});
+
+var doStart = function() {
+  timerID = setInterval(updateTime, 1000);
+}
+var updateTime = function() {
+  document.getElementById("timer").innerHTML = seconds;
+  seconds+=1;
+}
+var doReset = function() {
+  seconds = 0;
+  clearInterval(timerID);
+  document.getElementById("timer").innerHTML = "Stop Watch";
+}
+var doPause = function() {
+  clearInterval(timerID);
+}
