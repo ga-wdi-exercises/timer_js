3a9b3232a4a499e2be079ed64f6bc9e79f2f0a91
Kristin Pasive <kristin.pasive@gmail.com>
2016-06-01 13:20:19 -0400

    finished timer

--- timers.js
+++ timers.js
@@ -1,4 +1,30 @@
+$(document).ready(function(){ /*code in here*/
+
+var start = $("#start");
+var stop = $("#reset");
+var pause = $("#pause");
+var timer = 0;
+
+start.on("click", somethingElse);
+
+function startTimer(){
+  timer+=1;
+  $("h1").html("Time elapsed: " + timer);
+};
+
+function somethingElse(){
+awesomeTimer = setInterval(startTimer, 1000);
+}
+
+stop.on("click", function(){
+clearInterval(awesomeTimer);
+$("h1").html("Stop Watch");
+timer=0;
+})
+
+pause.on("click", function(){
+ clearInterval(awesomeTimer);
+})
 
 
-
-
+})
