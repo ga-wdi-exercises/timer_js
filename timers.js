f3828998a4fcc18a663af5142b9b0624bde159c9
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-01 21:34:43 -0400

    Timer Success

--- timers.js
+++ timers.js
@@ -3,37 +3,13 @@ $("#start").on("click",function updateTime(){
   var count = setInterval(function(){
     time= time + 1;
     console.log(time);
-    $("#timer").text(time);
+    $("#timer").text(time + " s");
   },1000)
   $("#pause").on("click",function pauseClock(){
     clearInterval(count);
 })
   $("#reset").on("click",function resetClock(){
     clearInterval(count);
-    $("#timer").text(0);
+    $("#timer").text(0 + "s");
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function resetClock(){
-//   console.log("Clock Reset");
-// }
-// function pauseClock() {
-//   console.log("Clock Paused");
-// }
