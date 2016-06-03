16edcffc873293460550f1540be185f335543f6a
James Hughes <heezmagnif@gmail.com>
2016-06-03 14:30:58 -0400

    Added Pause and Reset function

--- timers.js
+++ timers.js
@@ -1,36 +1,34 @@
+$(document).ready(function(){
+  var startButton = $("#start")
+  var stopWatch = $("#timer")
+  var seconds = 0;
+  var pauseButton = $("#pause")
+  var timerId
+  var resetButton = $("#reset")
 
-var stopwatch = {
-  seconds: 0,
-  timerId: null,
-  elements: {
-    startButton: document.getElementById("start"),
-    pauseButton: document.getElementById("pause"),
-    resetButton: document.getElementById("reset"),
-    h1: document.getElementsByTagName("h1")[0]
-  },
-  //This associates the javascript button actions to the html
-  listen: function(){
-  //listens to the event of the start button click
-    this.elements.startButton.addEventListener("click",function(){
-      if(!this.timerId){
-	this.timerId = setInterval(function(){
-	    this.elements.h1.innerHTML = "Time Elapsed: " + this.seconds
-	    this.seconds++ // adds a second as the seconds pass
-	}.bind(this),1000) // set interval
-      }
-    }.bind(this))
 
-    this.elements.pauseButton.addEventListener("click",function(){
-      this.timerId = clearInterval(this.timerId)
-    }.bind(this))
-    //listens for the click event on the pause button
+  //When I click on the start button
+  startButton.on("click",function(){
+    //change stopwatch to time elapse zero
+    stopWatch.html("Time elapsed" + " : " + seconds)
+      //every second
+      timerId=setInterval(function(){
+        seconds = (seconds + 1)
+        stopWatch.html("Time elapsed" + " : " + seconds)
+      },1000)
+        //add a second
+          //update time elapsed on the page
+  })
 
-    this.elements.resetButton.addEventListener("click", function(){
-      this.timerId = clearInterval(this.timerId)
-      this.elements.h1.innerHTML = "Stop Watch"
-      this.seconds = 0
-    }.bind(this))
-  }//listens for the click event on the reset button
-}
-stopwatch.listen()
-//makes the whole stopwatch function listen for clicks on all buttons
+    pauseButton.on("click",function(){
+      clearInterval(timerId)
+    })
+
+    resetButton.on("click",function(){
+      stopWatch.html("Stop Watch")
+      clearInterval(timerId)
+      seconds = 0
+    })
+
+
+});
