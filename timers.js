b7b9bef8705261da0dad97903174d6f51bd6f169
Megan Coyle <meg@mcoyle.com>
2016-07-05 14:07:29 -0400

    Add timer functionality

--- timers.js
+++ timers.js
@@ -1,4 +1,14 @@
+var startButton = document.getElementById("start");
+var timerText = document.getElementById("timer");
+var timeCount = 0;
 
+startButton.addEventListener("click", startTimer);
 
+function startTimer() {
+  setInterval(timeCountStart,1000);
+}
 
-
+function timeCountStart() {
+  timeCount.textContent = timeCount++;
+  timerText.textContent = "Time elapsed:" + " " + timeCount;
+}
