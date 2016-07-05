5b581e175f2714dac40453acfbc6e961cfc456f9
Megan Coyle <meg@mcoyle.com>
2016-07-05 16:14:42 -0400

    Add pause and reset functionality

--- timers.js
+++ timers.js
@@ -1,14 +1,36 @@
 var startButton = document.getElementById("start");
 var timerText = document.getElementById("timer");
 var timeCount = 0;
+var resetButton = document.getElementById("reset");
+var pauseButton = document.getElementById("pause");
+var timeInterval;
 
 startButton.addEventListener("click", startTimer);
+resetButton.addEventListener("click", resetTimer);
+pauseButton.addEventListener("click", pauseTimer);
+
+// var setTimer = setInterval(startTimer,1000);
+//
+// function startTimer() {
+//   timeCount.textContent = timeCount++;
+//   timerText.textContent = "Time elapsed:" + " " + timeCount;
+// }
 
 function startTimer() {
-  setInterval(timeCountStart,1000);
+  timeInterval = setInterval(timeCountStart,1000);
 }
 
 function timeCountStart() {
   timeCount.textContent = timeCount++;
   timerText.textContent = "Time elapsed:" + " " + timeCount;
+}
+
+function resetTimer() {
+  clearInterval(timeInterval);
+  timerText.textContent = "Stop Watch";
+  timeCount = 0;
+}
+
+function pauseTimer() {
+  clearInterval(timeInterval);
 }
