634e2220678fb48e71faac9193e8eb7e0c615eb9
dfwg4@hotmail.com <dfwg4@hotmail.com>
2016-11-26 14:13:37 -0500

    more code still not working

--- timers.js
+++ timers.js
@@ -3,30 +3,61 @@
 var timer = {
   seconds: 0,
   timerId: 0,
-}
+//};
 
 //reference the buttons made in html:timer???? - not a button/reset/start/pause
-var theButtons {
-this.reset = document.getElementById("timer"),
-this.reset = document.getElementById("reset"),
-this.start = document.getElementById("start"),
-this.pause = document.getElementById("pause"),
+theButtons: {
+  timer: document.getElementById("timer"),
+  reset: document.getElementById("reset"),
+  start: document.getElementById("start"),
+  pause: document.getElementById("pause"),
 },
 //create 'clickhandlers'
 //onClick do something
-('timer').onClick = (//function// , )  ;
-('reset').onClick = (//function// , )  ;
-('start').onClick = (//function// , )  ;
-('pause').onClick =  (//function// , ) ;
+//('timer').onClick = (//'updateTime()'// , )  ;
+//('reset').onClick = (//'resetTime()'// , )  ;
+//('start').onClick = (//'beginTime'// , )  ;
+//('pause').onClick =  (//'stopTime()'// , ) ;
+
+//Commented out about do one on one
 
 //instatiate - work with object - set Timer setInterval()
-function reset(){
- //return text to Stop Watch
-
-
+//return text to Stop Watch
 //make an update time method that inserts itself into <h1 id="timer">
-  function updateTime(){('Time elaspsed: ' + this.seconds)
-seconds++
-  };
+
 ////replace hmtl
-document.getElementById("timer").innerHtml("seconds"),
+//document.getElementById("timer").innerHtml = "seconds";
+updateTime: function updateTime(){
+  this.seconds++;
+  this.theButtons.getElementById("#timer").innerHTML = ('Time elaspsed: ' + this.seconds);
+//seconds++
+},
+
+ clickHandlers: function(){
+   this.theButtons.start.onClick('click', function(){
+      this.theButtons.getElementById("#timer").innerHTML = ('Time elaspsed: ' + this.seconds);
+      this.timerId = setInterval(this.updateTime.bind(this), 1000);
+ }.bind(this));
+
+
+ //function stopTime(){
+    this.theButtons.pause.onClick('click', function(){
+      clearInterval(this.timerId);
+    }.bind(this));
+
+//innerHTML = ("Time elapsed: " + 0);
+//setTimeout()
+//};
+
+    this.theButtons.reset.onClick ('click',function (){
+      this.seconds = 0;
+      clearInterval(this.timerId);
+      this.theButtons.getElementById('#timer').innerHTML = ("Stop Watch");
+//document.getElementById("#timer").innerHTML = ("Stop Watch");
+//clearInterval()
+ }.bind(this));
+
+  }
+};
+
+timer.clickHandlers();
