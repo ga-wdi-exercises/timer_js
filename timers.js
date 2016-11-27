48a3b930cd5031117094f31b0df66390c39ce0db
myztajay <myztajay@hotmail.com>
2016-11-27 13:52:45 -0500

    Create Timer, bugs: multiple instances of start time

--- timers.js
+++ timers.js
@@ -1,4 +1,39 @@
+$( document ).ready(function(){
+// timer & buttons
+let $timer = $(timer);
+let $reset = $(reset);
+let $start = $(start);
+let $pause = $(pause);
+// timeout variables
+let seconds = 0;
+let timerId;
+
+$reset.on('click', resetTime);
+$start.on('click', updateTime);
+$pause.on('click', pauseTime);
+
+// Function that is called when START is clicked
+function updateTime(){
+  timerId = setInterval(updateSeconds ,1000);
+  // updates seconds variable and updates stopwatch H1
+  function updateSeconds(){
+    seconds += 1;
+    console.log(seconds);
+    $timer.html(seconds);
+  }
+}
+
+// Function that is called when PAUSE is clicked
+function pauseTime(){
+  clearInterval(timerId);
+}
+
+// Funtion CLEARS seconds & interval, also reset "Stop Watch"
+function resetTime(){
+  clearInterval(timerId);
+  seconds = 0;
+  $timer.html("Stop Watch");
+}
 
 
-
-
+});//Doucment ready end
