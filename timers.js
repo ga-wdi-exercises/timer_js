d1dda71c0aef892eb55f6b9c19f9997f076b184c
Josh Foster <josh@allenb.com>
2016-11-26 21:24:27 -0600

    completed basic timer

--- timers.js
+++ timers.js
@@ -1,4 +1,55 @@
 
+var time = 0;
+
+addTime = function(){
+  time = time+1;
+  $('#timer').html(time);
+  return time;
+}
+resetTime = function(intervalTime){
+  clearInterval(intervalTime);
+  time = 0;
+  $('#timer').html(time);
+}
+
+startTime = function(){
+  var intervalTime = setInterval(addTime,1000);
+  $('#pause').on('click',function(){
+    clearInterval(intervalTime);
+  });
+  $('#reset').on('click',function(){
+    resetTime(intervalTime);
+  })
+}
+
+$('#start').on('click', startTime);
 
 
-
+// var Timer = {
+//
+// time: 0,
+//
+// addTime : function(){
+//   time = time+1;
+//   $('#timer').html(time);
+//   return time;
+// },
+// resetTime : function(intervalTime){
+//   clearInterval(intervalTime);
+//   this.time = 0;
+//   $('#timer').html(this.time);
+// },
+//
+// startTime : function(){
+//   var intervalTime = setInterval(this.addTime,1000);
+//   $('#pause').on('click',function(){
+//     clearInterval(this.intervalTime);
+//   });
+//   $('#reset').on('click',function(){
+//     resetTime(this.intervalTime);
+//   });
+// }
+//
+// };
+//
+// $('#start').on('click', Time.startTime);
