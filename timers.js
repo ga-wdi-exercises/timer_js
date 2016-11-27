7014b823f7c165db57c59a1e5adaa0d5235c3ecb
jtellis <jessetellis@gmail.com>
2016-11-27 14:19:40 -0500

    Add response

--- timers.js
+++ timers.js
@@ -1,4 +1,30 @@
+"use strict";
 
+const timer = {
+    seconds: 0,
+    id: undefined,
+    $timer: $("#timer"),
+    $reset: $("#reset"),
+    $start: $("#start"),
+    $pause: $("#pause"),
+    update: function() {
+        this.seconds += 1;
+        this.$timer.text(this.seconds);
+    },
+};
 
+$(function() {
+    timer.$reset.on("click", function(event) {
+        clearInterval(timer.id);
+        timer.seconds = 0;
+        timer.$timer.text("Stop Watch");
+    });
 
+    timer.$start.on("click", function(event) {
+        timer.id = setInterval(timer.update.bind(timer), 1000);
+    });
 
+    timer.$pause.on("click", function(event) {
+        clearInterval(timer.id);
+    });
+});
