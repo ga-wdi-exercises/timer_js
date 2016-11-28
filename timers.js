4582fb9f2747a7b260b96e9e2177889099f01162
Stephanie Gage <stephig85@gmail.com>
2016-11-28 08:11:36 -0600

    this one is not pretty, or running.

--- timers.js
+++ timers.js
@@ -1,4 +1,27 @@
+$(document).ready(function(){
 
+var seconds = 0;
+var timerID = ;
 
+function updateTime () {
+  seconds++;
+}
 
+  $('#reset').click() {
 
+  });
+
+  $('#start').click() {
+    var timerID = window.setInterval(function, 1000[param1, param2]);
+  });
+
+  $('#pause').click() {
+
+  });
+
+function timer(){
+
+}
+
+// really not sure what to do on this one... :/ 
+});
