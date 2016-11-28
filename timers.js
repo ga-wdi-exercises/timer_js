98889c9f53e75239778b8254f069a1d2ddb3c691
Namanda Musoke <nvmusoke@gmail.com>
2016-11-28 09:59:01 -0600

    finished-timer

--- timers.js
+++ timers.js
@@ -1,4 +1,42 @@
 
+  var start =('#start');
+  var reset =('#reset');
+  var pause =('#pause');
+  var timer =('#timer');
+  var count = 0;
+  var timerRunning = false;
+
+  var seconds;
+  var timerId;
 
 
+  function updateTime(){
+    if(timerRunning === false){
+    $(timer).text(count);
 
+    seconds = setInterval(function(){
+      count++;
+      $(timer).text(count);
+    },1000);
+
+    timerRunning = true;
+  }
+}
+
+  function pauseTime() {
+    clearInterval(seconds);
+    timerRunning = false;
+  }
+  
+  function resetTime() {
+    pauseTime();
+    count = 0;
+    $(timer).text(count);
+    timerRunning = false;
+  }
+
+  $(start).on('click', updateTime);
+
+  $(reset).on('click',resetTime);
+
+  $(pause).on('click', pauseTime);
