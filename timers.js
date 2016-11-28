b74e0b41f809507c833c55e916d65b6459dab6d0
brittshroyer <britt.shroyer@gmail.com>
2016-11-28 09:51:20 -0600

    buttons working

--- timers.js
+++ timers.js
@@ -1,4 +1,33 @@
+var seconds;
+var timerId;
+var timer = '#timer';
+var count = 0
+
+
+function updateTime(){
+  seconds = setInterval(function(){
+    $(timer).text(count);
+    count++;
+
+
+  }, 1000);
+}
+
+function pauseTime(){
+  clearInterval(seconds);
+}
+
+function resetTime(){
+  pauseTime();
+  count = 0;
+  $(timer).text(count);
+}
 
 
 
 
+$('#start').on('click', updateTime);
+
+$('#pause').on('click', pauseTime);
+
+$('#reset').on('click', resetTime);
