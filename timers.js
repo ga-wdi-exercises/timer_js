ea87cee83904fbdbb93828fa4ca764a33ed6fbd4
dan-nguyen <dtn601@gmail.com>
2016-11-27 21:34:30 -0600

    timer done

--- timers.js
+++ timers.js
@@ -1,4 +1,45 @@
+window.onload = function(){
+console.log("setup");
+ 
+var	timer = document.getElementById('timer'),
+	reset = document.getElementById('reset'),
+	start = document.getElementById('start'),
+	pause = document.getElementById('pause'),
+	seconds = 00,
+	t;
+
+start.onclick = function (){
+	clearInterval(t);
+	t = setInterval(stopwatch, 100);
+	timer.innerHTML = "Time elapsed: " + "0" + seconds;	
+
+};
+
+pause.onclick = function(){
+
+	clearInterval(t);
+
+};
+
+reset.onclick = function(){
+	clearInterval(t);
+	seconds = "00";
+	timer.innerHTML = "Stop Watch";
+}
 
 
 
+function stopwatch(){
+	seconds++;
+	if (seconds <= 9){
+		timer.innerHTML = "Time elapsed: " + "0" + seconds;
+		
+	}
+	if (seconds > 9){
+		timer.innerHTML = "Time elapsed: " + seconds;
+	}
 
+}￼
+
+
+}
