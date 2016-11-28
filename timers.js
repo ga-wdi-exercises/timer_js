c023d8d33189245a2ed164210001c65190aa86d6
yourUsername <your_email@example.com>
2016-11-27 22:22:10 -0500

    attempted js

--- timers.js
+++ timers.js
@@ -1,4 +1,39 @@
+//Javascript selectors for timer buttons
+
+let reset = document.getElementById('Reset');
+let start = document.getElementById('Start');
+let pause = document.getElementById('pause');
+
+// let reset = $("reset");
+// let start = $("start");
+// let pause = $("pause");
+
+// function Timer(reset,start,pause) {
+// this.reset = "reset";
+// this.start = "start";
+// this.pause = "pause";
+// }
+
+//Click handlers for each timer button
+$("#reset").click(function() {
+  alert( "#" );
+});
+
+$( "#start" ).click(function() {
+  alert( "#" );
+});
+
+$( "#pause" ).click(function() {
+  alert( "#" );
+});
+
+//Instantiate 'seconds' and 'timerId' variables
+let seconds = 0;
+let timerId;
+
+//updateTime() function incrementing 'seconds' counter and inserts
+//value into the <h1> element with 'id="timer"'
 
 
 
-
+}
