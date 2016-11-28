4c94fd7ad5a8f23ccf3e9cb1f7dc72f15d99667f
Will Sparger <spargerw@gmail.com>
2016-11-27 23:21:21 -0600

    Unfinished timer

--- timers.js
+++ timers.js
@@ -1,4 +1,22 @@
+$(function() {
 
 
+var seconds = i;
+var timerId = "Time elapsed" + seconds;
 
+  
+  $('#start').on('click', function(){
+    var updateTime = function() {
 
+            timerId = setInterval(updateTime, 1000);
+
+        $('#timer').text(timerId);
+    };
+    updateTime();
+  });
+
+  $('#reset').on('click', function(){
+    $('#timer').text('Stop Watch');
+  });
+
+});
