049c93ae8fb22b3bd66a1b183a8b450b8647781e
Stephanie Workman <sworkman2014@gmail.com>
2016-11-27 18:36:12 -0600

    added JS for stopwatch but not fully functional

--- timers.js
+++ timers.js
@@ -1,4 +1,41 @@
+$ (function(){
 
+  var seconds = 0;
+  var minutes = 0;
+  var hours = 0;
+  var timerId;
 
+  function updateTime(){
+    seconds++;
+    if (seconds>59){
+      seconds=0;
+      minutes++;
+      if (minutes>59){
+        minutes=0;
+        hours++;
+      }
+    }
+  }
 
+$('#start').on('click', function(){
+  console.log('start timer');
+  var timerId = setInterval(updateTime, 1000);
+  // document.getElementById('timer').innerHTML = "Time elapsed: "+hours+": "+minutes+": "+seconds;
+  $('#timer').text("Time elapsed: " + "0" + hours + ":" + "0" + minutes + ":" + seconds + "0");
+});
 
+$('#pause').on('click', function(){
+  console.log('pause timer');
+  clearInterval(timerId);
+});
+
+$('#reset').on('click', function(){
+  console.log('reset timer');
+  clearInterval(timerId);
+  seconds=0;
+  minutes=0;
+  hours=0;
+  $('#timer').text("Time elapsed: " + hours + " : " + minutes + ":" + seconds);
+});
+
+});
