2f18398a2d1dd7151c19e9dec9d9b48d63050a86
Gina Fitzgerald <g24fitzgerald@gmail.com>
2016-11-27 20:23:41 -0600

    attempted to create timer

--- timers.js
+++ timers.js
@@ -1,4 +1,28 @@
+$(function() {
 
+  var second = 0;
+  var intervalId;
 
+  $('#reset').on('click', function(event){
+    console.log('reset was clicked');
+  });
 
+  $('#start').on('click', function(event){
+    console.log('start was clicked');
+    intervalId = setInterval(function(){
+      second++;
+          });
+      //change timer value to counter
+      $('#timer').text('Time Elapsed: ');
+      $('#counterTime').val(intervalId).show();
+  });
 
+  $('#pause').on('click', function(event){
+    console.log('pause was clicked');
+  });
+
+  //reset function
+  var resetTimer = function(event){
+    clearInterval(intervalId);
+  }
+});
