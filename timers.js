3a900f945a517f633ea8d08eaeccff97a0d11337
Stephen Landis <stephen.n.landis@gmail.com>
2016-11-27 18:42:32 -0600

    timer hw

--- timers.js
+++ timers.js
@@ -1,4 +1,14 @@
+console.log('app.js is loaded');
 
+$(function() {
+     //all javascript goes here
 
+$('#start').on('click', function(event) {
+  setInterval()
+} ('#reset').on('click', function(event) {
+  clearTimeout()
+} ('#pause').on('click', function(event) {
+  setTimeout()
+}
 
-
+});
