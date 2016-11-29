9502c2d91d24073d041f92aa4752d24563fac816
christopher barragan <barraganchris95@gmail.com>
2016-11-29 07:14:51 -0600

    Start button works, pause doesn't

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+window.onload = function() {
+  console.log("setup");
 
+  var timer = document.getElementById('timer'),
+      reset = document.getElementById('reset'),
+      start = document.getElementById('start'),
+      pause = document.getElementById('pause'),
+      seconds = 0;
 
+      /* setInterval function */
+      start.onclick = function() {
+        var seconds = window.setInterval(function() { updateTime() }, 1000);
+        function updateTime() {
+                seconds++;
+                $('#timer').text("Time Elapsed: " + seconds);
+          }
+                $('#timer').innerHTML(seconds);
+      }
 
+      pause.onclick = function pauseTime() {
+          window.clearInterval(seconds);
+          $('#pause').on('click', pauseTime);
+      }
 
+      reset.onclick = function() {
+
+      }
+
+};
