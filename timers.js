bc5cdceab3b0bba0e82c82c7d22940b4794275d2
ahnuce <ahnuce@gmail.com>
2017-03-07 10:51:16 -0500

    simple style

--- timers.js
+++ timers.js
@@ -1,5 +1,3 @@
-
-
 let reset = document.getElementById('reset');
 let start = document.getElementById('start');
 let pause = document.getElementById('pause');
@@ -7,30 +5,27 @@ let pause = document.getElementById('pause');
 let seconds = 0;
 let timerId;
 
-start.addEventListener('click', function(){
-  startCount = setInterval(updateTime, 1000);
+start.addEventListener('click', function() {
+    startCount = setInterval(updateTime, 1000);
 });
 
-pause.addEventListener('click', function(){
-  clearInterval(startCount);
+pause.addEventListener('click', function() {
+    clearInterval(startCount);
+
 });
 
-reset.addEventListener('click', function(){
-  seconds = 0;
-  resetCount = clearInterval(startCount);
-  document.getElementsByTagName('h1')[0].innerHTML="Stop Watch";
+reset.addEventListener('click', function() {
+    seconds = 0;
+    resetCount = clearInterval(startCount);
+    document.getElementsByTagName('h1')[0].innerHTML = "Stop Watch";
 });
 
 
 
-function updateTime(){
- // for (let i = 0; i<20; i++){
+function updateTime() {
+    document.getElementsByTagName('h1')[0].innerHTML = seconds;
+    seconds++;
 
-   document.getElementsByTagName('h1')[0].innerHTML=seconds;
-   seconds++;
-
-
- // }//for
 }
 
 //updateTime function
